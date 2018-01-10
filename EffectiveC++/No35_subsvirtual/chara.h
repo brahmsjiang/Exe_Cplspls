7a619f4a1688197f66022e494d9377bdf24e80d5
brahmsjiang <brahmsjiang@163.com>
2018-01-10 23:53:54 +0800

    update no35

--- EffectiveC++/No35_subsvirtual/chara.h
+++ EffectiveC++/No35_subsvirtual/chara.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class chara{
@@ -27,22 +28,21 @@ class beggar1:public character1{
 };
 /////////////////////////////////////////////////////////
 
-class character2;
-int default_health(const character2& gc){
-	return gc.getrawhealthval();
-}
+class character2;	//statment for below func statement
+//at this position, default_health can only statement and cannot be defined becasue compiler think character2's define is not complete for it is used as a parmeter(quote and pointer is the same)
+//==================>invalid use of incomplete type 
+int default_health(const character2& gc);
 
 class character2{
 public:
 	typedef int (*healthcalfunc) (const character2&);
-	explicit character2(healthcalfunc hcf=default_health, int rawval)
+	explicit character2(healthcalfunc hcf=default_health, int rawval=100)//defaut par must initialize from right side
 		:healthfunc(hcf),
 		rawhealthval(rawval)
-		
 	{
 
 	}
-	int getrawhealthval(){
+	int getrawhealthval() const{	//must const,  const obj can only call const func as defalut_health=======>passing ‘const *’ as ‘this’ argument of ‘*’ discards qualifiers
 		return rawhealthval;
 	}
 	int healthval() const{
@@ -52,4 +52,46 @@ private:
 	int rawhealthval;
 	healthcalfunc healthfunc;
 };
+
+
+int default_health(const character2& gc){
+	gc.getrawhealthval();
+}
+
+class badguy:public character2{
+public:
+	explicit badguy(std::string str="", healthcalfunc hcf=default_health, int rawval=100)
+		:character2(hcf,rawval)//子类fish的构造函数中，加上一个冒号(:)，然后加上父类的带参构造函数，这就是父类构造函数的显式调用
+	{
+		sname(str);
+	}
+	string& getname(){
+		return sname;
+	}
+private:
+	std::string sname;
+
+};
+
+
+class test{
+public:
+	test(string str=""):
+		sname(str){
+
+		}
+	void print(){
+		cout<<sname<<endl;
+	}
+private:
+	string sname;
+};
+
+
+
+
+
+
+
+
 
--- EffectiveC++/No35_subsvirtual/main.cpp
+++ EffectiveC++/No35_subsvirtual/main.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+
+int bosshealth(const badguy& obj){
+	return obj.getrawhealthval()+20;
+};//func, such as main func cannot define func, except c++11 lambda
+
+
 int main(int argc, char* argv[])
 {
 	character1* cp=new beggar1();
@@ -11,6 +17,15 @@ int main(int argc, char* argv[])
 	cp=new character1();
 	cp->healthval();
 	//////////////////////////////////////////////
-	
+	//std::string name="zhaojia";
+	//badguy boss0;// default construct should not add boss0(), complier will regard it as a pointer initialization====>error: request for member '*' in '*', which is of non-class type '*'
+	std::string tmp("zhaojia");
+	badguy boss0(tmp,bosshealth,200);
+	cout<<"boss name: "<<boss0.getname()<<endl;
+	cout<<"boss health val: "<<boss0.healthval()<<endl;
+
+
+	test pp("ddd");
+	pp.print();	
 	return 0;
 }
