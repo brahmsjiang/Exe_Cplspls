cd020106c8c4f9366aff26726717577cb65e4dc3
brahmsjiang <brahmsjiang@163.com>
2018-01-29 21:49:16 +0800

    update no41
    Change-Id: I333e9d0fb3465ea515c16af6fa0b0359d1e53a19

--- EffectiveC++/No41_templategenericprog/main.cpp
+++ EffectiveC++/No41_templategenericprog/main.cpp
@@ -1,14 +1,27 @@
 #include <algorithm>	//swap
 #include <iostream>
 #include <string>
-
+#define someNastyWidget 128
 using namespace std;
+
+struct Mem
+{
+	Mem(int num)
+		:mmem(num){}
+	int mmem;
+};
+
 
 class Widget
 {
 public:
 	Widget(std::string name="default", std::size_t size=0)
 		:sname(name), ssize(size)
+	{
+
+	}
+	Widget(std::size_t size=0)
+		:sname("default"), ssize(size)
 	{
 
 	}
@@ -35,12 +48,55 @@ public:
 private:
 	std::string sname;
 	std::size_t ssize;
+};
 
+class WidgetSp
+{
+public:
+	WidgetSp(std::string name="default", const Mem& Memobj=Mem(0))
+		:sname(name), smem(Memobj)
+	{
+
+	}
+	WidgetSp(const Mem& Memobj=Mem(0))
+		:sname("default"), smem(Memobj)
+	{
+
+	}
+	WidgetSp(int num)
+		:sname("default"), smem(Mem(num))	//it only canbe convert once, int->Mem->WidgerSp is prohibit
+	{
+
+	}
+	virtual ~WidgetSp(){
+
+	}
+	void print() const{
+		cout<<"sname: "<<sname<<"	ssize:"<<smem.mmem<<endl;
+	}
+	const Mem& size() const{
+		return smem;
+	}
+	virtual void normalize(){
+		sname="===="+sname+"===";
+		smem.mmem/=smem.mmem;
+	}
+	void swap(WidgetSp& other){
+		std::swap(this->sname, other.sname);
+		std::swap(this->smem.mmem, other.smem.mmem);
+	}
+
+private:
+	std::string sname;
+	Mem smem;
 
 };
 
-//class someNastyWidget;
-Widget someNastyWidget;
+
+
+//Widget someNastyWidget;
+//WidgetSp someNastyWidget;
+
 
 
 template<typename T>
@@ -53,21 +109,34 @@ void doProcessing(T& w)
 	}
 }
 
-
-class someNastyWidget
+bool operator>(const Mem& lhs, const Mem& rhs)
 {
+	return (lhs.mmem > rhs.mmem);
+}
 
-};
+bool operator!=(const WidgetSp& lhs, const WidgetSp& rhs){
+	return true;
+}
+
 
 int main()
 {
-	Widget wg0("fuck",20);
+
+	Widget wg0("fffff",20);
 	doProcessing(wg0);
 	wg0.print();	
-	Widget wg1("fuck",2);
+	Widget wg1("fffff",2);
 	doProcessing(wg1);
-	wg1.print();	
+	wg1.print();
 
+	cout<<"------------"<<endl;
+	WidgetSp wgs0("shit",3);
+	doProcessing(wgs0);
+	wgs0.print();
+	WidgetSp wgs1("shit",13);
+	doProcessing(wgs1);
+	wgs1.print();
 
 	return 0;
 }
+
