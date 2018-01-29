615dbaaf619d934c50c3467a94c966ced1ee3c11
brahmsjiang <brahmsjiang@163.com>
2018-01-29 21:57:09 +0800

    update no42
    Change-Id: I1e8ff99b1692f8a9603b503efe9bf3878f112a71

---
+++ EffectiveC++/No42_understandtypename/main.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2018年01月29日 星期一 21时52分47秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+template<typename T> class Widget;
+template<class T>class Widget;
+
+
+int main()
+{
+	return 0;
+}
