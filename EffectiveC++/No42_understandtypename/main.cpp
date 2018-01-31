fce3fbd4a05b8616830db4007f0bf1c7dd1fd912
brahmsjiang <brahmsjiang@163.com>
2018-01-31 18:35:01 +0800

    update no42
    Change-Id: Ic31bf6ccab354067769bbd6501cf62b7fe89a877

--- EffectiveC++/No42_understandtypename/main.cpp
+++ EffectiveC++/No42_understandtypename/main.cpp
@@ -6,14 +6,38 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
 
 template<typename T> class Widget;
 template<class T>class Widget;
 
+template<typename C>
+void print2nd(const C& container)
+{
+	if(container.size() >= 2) {
+		typename C::const_iterator iter(container.begin());	//nested dependent name  
+		++iter;
+		int val = *iter;	//non-independent
+		cout<<val<<endl;
+
+		//C::const_iterator* x;	//error, need typename, default isnt a typename regarded by compiler 
+	}
+}
+
+template<typename C>
+void func0(const C& container, C::iterator iter)	//1st para cannot add typename, 2st para must add typename
+{
+
+}
 
 int main()
 {
+	vector<int> vi0(10,5);
+	vector<int> vi1(10);
+	print2nd(vi0);
+	print2nd(vi1);
 	return 0;
 }
