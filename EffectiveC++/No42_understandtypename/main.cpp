3e0b26286bdb1e7f8cccf0a1fcf4a66a643dbe71
brahmsjiang <brahmsjiang@163.com>
2018-02-01 20:48:23 +0800

    update no42,43
    Change-Id: I6bb0f7ecfd71289c5e1a9c998b4b5346e727d5ca

--- EffectiveC++/No42_understandtypename/main.cpp
+++ EffectiveC++/No42_understandtypename/main.cpp
@@ -7,6 +7,9 @@
 
 #include<iostream>
 #include<vector>
+#include<list>
+#include<string>
+#include<typeinfo>
 
 using namespace std;
 
@@ -39,21 +42,35 @@ void func0(const C& container, typename C::const_iterator iter)	//1st para canno
 }
 
 template<typename T>
-class Base<T>
+class Base
 {
-	struct Nested{
-
+public:
+	class Nested{
+	public:
+		Nested(int var=0)
+			:x(var){}
+		void printNes(){
+			cout<<"Base<"<<typeid(T).name()<<"> Nested x: "<<x<<endl;
+		}
+		int x;
+	};
+	void printBase(){
+		cout<<"printBase"<<endl;
 	}
-
+	
 };
 
 template<typename T>
-class Derived:public Base<T>::Nested {
+class Derived:public Base<T>::Nested{ 	//base class list, forbid typename
 public:
+	typedef typename Base<T>::Nested BTN;
 	explicit Derived(int x)
-		:Base<T>Nested(x)
+		//:Base<T>::Nested(x)	//member init list, forbid typename
+		:BTN(x)
 	{
-		typename Base<T>::Nested temp;
+		//typename Base<T>::Nested temp;
+		BTN temp(77);
+		temp.printNes();
 	}
 };
 
@@ -66,6 +83,17 @@ int main()
 	vector<int>::iterator iter1=vi1.begin();
 	//vector<int>::iterator iter1;
 	func0(vi1,iter1);
+
+	Derived<list<char>> der0(10);
+	//der0.printBase();	//err,class Derived isnt inheritted from Base, but from Nested
+	der0.printNes();	//so it can call bases Nested func
+
+	Base<int>::Nested nes0(20);
+	nes0.printNes();
+	Base<std::string>::Nested nes1(30);
+	nes1.printNes();
+	Base<vector<char>>::Nested nes2(40);
+	nes2.printNes();
 	return 0;
 }
 
---
+++ EffectiveC++/No43_accessnameintemplatizedbaseclass/main.cpp
@@ -0,0 +1,161 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2018年01月29日 星期一 21时52分47秒
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<list>
+#include<string>
+#include<typeinfo>
+
+using namespace std;
+
+class CompanyA{
+public:
+	void sendClrText(const std::string& str)
+	{
+		cout<<"compA::sendClrText==>"<<str.data()<<endl;
+	}
+	void sendEncrypted(const std::string& str)
+	{
+		cout<<"compA::sendEncrypted==>"<<str.data()<<endl;
+	}
+
+};
+
+class CompanyB{
+public:
+	void sendClrText(const std::string& str)
+	{
+		cout<<"compB::sendClrText==>"<<str.data()<<endl;
+	}
+	void sendEncrypted(const std::string& str)
+	{
+		cout<<"compB::sendEncrypted==>"<<str.data()<<endl;
+	}
+};
+
+class CompanyZ{
+public:
+	void sendEncrypted(const std::string& str)
+	{
+		cout<<"compZ::sendEncrypted==>"<<str.data()<<endl;
+	}
+};
+
+
+class MsgInfo{
+public:
+	MsgInfo(const std::string& msg)
+		:rmsg(msg)
+	{}
+	enum MSGLEVEL{	//declaration must add ";" at end, defination cannt add ";"
+		NON = 0,
+		ENC,
+	};
+	std::string createMsg(MSGLEVEL lev) const
+	{
+		std::string tmp;
+		switch(lev)
+		{
+			case NON:
+				tmp = rmsg;
+				break;
+			case ENC:
+				tmp = "******"+rmsg+"******";
+				break;
+			default:
+				break;
+		}
+		return tmp;
+	}
+	std::string rmsg;
+};
+
+template<typename Company>
+class MsgSender
+{
+public:
+	void sendClear(const MsgInfo& info){
+		std::string msg;
+		msg = info.createMsg(MsgInfo::NON);
+		Company c;
+		c.sendClrText(msg);
+	}
+	void sendSecret(const MsgInfo& info){
+		std::string msg;
+		msg = info.createMsg(MsgInfo::ENC);
+		Company c;
+		c.sendEncrypted(msg);
+	}
+
+};
+
+template<>	//total template specialization
+class MsgSender<CompanyZ>
+{
+public:
+	void sendSecret(const MsgInfo& info){
+		std::string msg;
+		msg = info.createMsg(MsgInfo::ENC);
+		CompanyZ c;
+		c.sendEncrypted(msg);
+	}
+};
+
+template<typename Company>
+class LoggingMsgSender:public MsgSender<Company>
+{
+public:
+	//using MsgSender<Company>::sendClear; 	//2nd. tell compiler to fine in base class scope
+	void sendClearMsg(const MsgInfo& info){
+		cout<<"logging start"<<endl;
+		//sendClear(info);	//err,compiler doesnt know MsgSender<company> whether has thisfunc or not
+		this->sendClear(info);	//1st, assume sendclear will be inheritted
+		//sendClear(info);	//2nd
+		//MsgSender<Company>::sendClear(info);//3rd,not good,if func is virtual,is will close virtual function
+		cout<<"logging end"<<endl;
+	}
+	void sendSecretMsg(const MsgInfo& info){
+		cout<<"logging start"<<endl;
+		this->sendSecret(info);
+		cout<<"logging end"<<endl;
+	}
+
+};
+
+int main()
+{
+	MsgSender<CompanyA> senda;
+	MsgInfo ma("PPPP");
+	senda.sendClear(ma);
+	senda.sendSecret(ma);
+
+	MsgSender<CompanyB> sendb;
+	MsgInfo mb("QQQQ");
+	sendb.sendClear(mb);
+	sendb.sendSecret(mb);
+
+	MsgSender<CompanyZ> sendz;
+	MsgInfo mz("RRRR");
+	//sendz.sendClear(mz);	//err
+	
+	LoggingMsgSender<CompanyA> logsenda;
+	MsgInfo lma("lma");
+	logsenda.sendClearMsg(lma);
+
+	LoggingMsgSender<CompanyZ> logsendz;//2nd will err, using...
+	MsgInfo lmz("lmz");
+	//logsendz.sendClearMsg(lmz);	//err whichever method is used
+	logsendz.sendSecret(lmz);
+
+	return 0;
+}
+
+
+
+
+
