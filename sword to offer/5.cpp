14aed2df601ae3fb3e4eb41d7c7a40470711f137
brahmsjiang <brahmsjiang@163.com>
2019-09-02 16:57:52 +0800

    update

--- sword to offer/5.cpp
+++ sword to offer/5.cpp
@@ -8,14 +8,56 @@
 using namespace std;
 
 
-void test(void*)
+void PrintListReversingly_Iteratively(ListNode* pHead)
 {
+    if(pHead == nullptr)
+        return;
+
+    stack<ListNode*> nodes;
+
+    ListNode* pNode = pHead;
+    nodes.push(pNode);
+    while(pNode->m_pNext != nullptr)
+    {
+        pNode = pNode->m_pNext;
+        nodes.push(pNode);
+    }
+    while(!nodes.empty())
+    {
+        cout<<(nodes.top())->m_nValue<<"\t";
+        nodes.pop();
+    }
+    cout<<endl;
+}
+
+void PrintListReversingly_Recursively(ListNode* pHead)
+{
+    ListNode* pNode = pHead;
+    if(pNode != nullptr)
+    {
+        PrintListReversingly_Recursively(pNode->m_pNext);
+        cout<<pNode->m_nValue<<"\t";
+    }
+}
+
+void test()
+{
+
+    ListNode* pHead = CreateListNode(1);
+    for(int i = 2; i <= 5; ++i)
+    {
+        AddToTail(&pHead, i);
+    }
+    PrintList(pHead);
+    PrintListReversingly_Iteratively(pHead);
+    PrintListReversingly_Recursively(pHead);cout<<endl;
 }
 
 
 
 int main(int argc, char* argv[])
 {
+    test();
 	
 	return 0;
 }
