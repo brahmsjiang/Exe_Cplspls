5089e8b46a20f42ed74d1bb874f6484c7611a851
brahmsjiang <brahmsjiang@163.com>
2019-12-24 22:59:55 +0800

    update c++

---
+++ sword to offer/3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <memory>
+#include <stack>
+#include <assert.h>
+#include <queue>
+#include "Utilities/List.h"
+
+using namespace std;
+
+bool duplicate(int numbers[], int length, int* duplication)
+{
+	if(numbers == nullptr || length <= 0)
+	{
+		return false;
+	}
+	duplication = nullptr;
+
+	for(int i = 0; i < length; i++)
+	{
+		while(numbers[i] != i){
+			int curval = numbers[i];
+			if(numbers[curval] != curval){
+				//swap numbers[i] and numbers[numbers[i]]
+				numbers[i] = numbers[curval];
+				numbers[curval] = curval;
+			}
+			else{
+				*duplication = curval;
+				return true;
+			}
+		}
+	}
+	return false;
+
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+    test();
+	
+	return 0;
+}
+
+
