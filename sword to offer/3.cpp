f68eb2138545cdb0d9bf61940e3a1f60dcb0147c
brahmsjiang <brahmsjiang@163.com>
2020-01-13 21:41:44 +0800

    update

--- sword to offer/3.cpp
+++ sword to offer/3.cpp
@@ -33,23 +33,47 @@ bool duplicate1(int numbers[], int length, int* duplication)
 	return false;
 }
 
-int countRange(const int* numbers, int length, int end)
+int countRange(const int* numbers, int length, int start, int end)
 {
+	int count = 0;
+	if (numbers == nullptr)
+		return count;
 
+	for (int i = 0; i < length; ++i)
+	{
+		if (numbers[i] >= start && numbers[i] <= end)
+			count++;
+	}
+	return count;
 }
 
 int duplicate2(const int* numbers, int length)
 {
-	if (numbers == nullptr || length <= 0)
+	if(numbers == nullptr || length <= 0)
 	{
 		return -1;
 	}
-	int scope = length - 1;
-	for (int i = 1; i <= scope; ++i)
-	{
-		
-	}
 
+	int start = 1;
+	int end = length - 1;
+	while(start <= end)
+	{
+		int mid = ((end - start) >> 1) + start;
+		int count = countRange(numbers, length, start, mid);
+		if (start == end)
+		{
+			if(count > 1)
+				return start;
+			else
+				break;
+		}
+
+		if (count > (mid - start + 1))
+			end = mid;
+		else
+			start = mid + 1;
+	}
+	return -1;
 }
 
 void test()
