80253fd8da65547049e302962d18a5013f061d32
ljiang <ljiang@telenav.cn>
2020-03-24 14:58:06 +0800

    update

--- sword to offer/19.cpp
+++ sword to offer/19.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
-bool matchCore(char* str, char* pattern)
+bool matchCore(const char* str, const char* pattern)
 {
 	if (*pattern == '\0' && *str == '\0')
 		return true;
-
+    // str is longer than pattern, mismatch absolutely
+    // however there' need to match when pattern's longer than str, for case: c[a]*\0 <=> ca[\0], '[]' means current char 
 	if (*pattern == '\0' && *str != '\0')
 		return false;
 
 	if (*(pattern + 1) == '*') {
-		if (*pattern == *str && *str != '0')
+		if (*pattern == *str || (*pattern == '.' && *str != '\0'))  // '.*' can't match '\0'
 			// move on the next state, both consume cur_char and 'a*', for case 'a*' matches cur_char once: aa <=> a*a or ab <=> a*b
 			return matchCore(str + 1, pattern + 2)
 			// stay on the current state, only consume cur_char, for case 'a*' matches a set of repeated cur_char: aaaab <=> a*b
@@ -27,16 +29,17 @@ bool matchCore(char* str, char* pattern)
 	return false;
 }
 
-bool match(char* str, char* pattern)
+bool match(string str, string pattern)
 {
-	if (nullptr == str || nullptr == pattern)
+	if (str.empty() || str.empty())
 		return false;
-	return matchCore(str, pattern);
+	return matchCore(str.c_str(), pattern.c_str());
 }
 
 
 int main(int argc, char* argv[])
 {
+#if 0
 	cout << match("", "") << endl;// true
 	cout << match("", ".*") << endl;// true
 	cout << match("", ".") << endl;// false
@@ -57,19 +60,16 @@ int main(int argc, char* argv[])
 	cout << match("aaa", "aa.a") << endl;// false
 	cout << match("aaa", "a.a") << endl;// true
 	cout << match("aaa", ".a") << endl;// false
-	cout << match("aaa", "a*a") << endl;// true!!!!!!!!!!
-	cout << match("aaaaa", "a*a") << endl;// true!!!!!!!!!!
+	cout << match("aaa", "a*a") << endl;// true
 	cout << match("aaa", "ab*a") << endl;// false
 	cout << match("aaa", "ab*ac*a") << endl;// true
-	cout << match("aaa", "ab*a*c*a") << endl;// true
-#if 0
-
-	Test("Test25", "aaa", ".*", true);
-	Test("Test26", "aab", "c*a*b", true);
-	Test("Test27", "aaca", "ab*a*c*a", true);
-	Test("Test28", "aaba", "ab*a*c*a", false);
-	Test("Test29", "bbbba", ".*a*a", true);
-	Test("Test30", "bcbbabab", ".*a*a", false);
+	cout << match("aaa", ".*") << endl;// true
 #endif
+    cout << match("aab", "c*a*b") << endl;// true
+    cout << match("aaca", "ab*a*c*a") << endl;// true
+    cout << match("aaba", "ab*a*c*a") << endl;// false
+    cout << match("bbbba", ".*a*a") << endl;// true
+    cout << match("bcbbabab", ".*a*a") << endl;// false
+
 	return 0;
 }
