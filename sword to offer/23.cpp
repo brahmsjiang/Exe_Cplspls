f37f5fd9d8335939288243a1426b1f231877ab37
ljiang <ljiang@telenav.cn>
2020-04-03 16:52:23 +0800

    AUTOREF-13637, update ndk mkfile and build.py to publish so

--- sword to offer/23.cpp
+++ sword to offer/23.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+ListNode* MeetingNode(ListNode* pHead)
+{
+    if (pHead == nullptr)
+        return nullptr;
+}
+
+ListNode* EntryNodeOfLoop(ListNode* pHead)
+{
+    if (pHead == nullptr)
+        return nullptr;
+}
+
 int main(int argc, char* argv[])
 {
 
