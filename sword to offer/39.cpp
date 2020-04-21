003b6c058e850f8bae3ce831b656d3ec3d36f6a6
brahmsjiang <brahmsjiang@163.com>
2020-04-21 22:30:30 +0800

    update

--- sword to offer/39.cpp
+++ sword to offer/39.cpp
@@ -2,47 +2,85 @@
 
 using namespace std;
 
-int MoreThanHalfNum(int* numbers, int length)
+int Partition(int* numbers, int len, int start, int end)
 {
-    if (CheckInvalidArray(numbers, length))
-        return 0;
+	int pivot = numbers[start];
+	int i = start, j = end, res = 0;
 
-    int pivot = numbers[0];
-    int i = 0, j = length - 1, res = 0;
+	while (i < j)
+	{
+		while (i < j && numbers[j] >= pivot)
+			--j;
+		if (i < j && numbers[j] < pivot)
+		{
+			numbers[i] = numbers[j];
+			++i;
+		}
 
-    while (i < j)
-    {
-        while (i < j && numbers[j] >= pivot)
-            --j;
-        if (i < j && numbers[j] < pivot)
-        {
-            numbers[i] = numbers[j];
-            ++i;
-        }
+		while (i < j && numbers[i] <= pivot)
+			++i;
+		if (i < j && numbers[i] > pivot)
+		{
+			numbers[j] = numbers[i];
+			--j;
+		}
+	}
+	numbers[i] = pivot;
+	return i;
+}
 
-        while (i < j && numbers[i] <= pivot)
-            ++i;
-        if (i < j && numbers[i] > pivot)
-        {
-            numbers[j] = numbers[i];
-            --j;
-        }
-    }
-    numbers[i] = pivot;
-    if (i == length / 2)
-        res = numbers[i];
-    else
-    {
-        res = MoreThanHalfNum() || MoreThanHalfNum();
-    }
-    return res;
+bool CheckInvalidArray(int* numbers, int length)
+{
+	if (numbers == nullptr || length <= 0)
+		return true;
+	return false;
+}
 
+bool CheckMoreThanHalf(int* numbers, int len, int number)
+{
+	int eqlNum = 0;
+	for (size_t i = 0; i < len; i++)
+	{
+		if (numbers[i] == number)
+			++eqlNum;
+	}
+	if ((eqlNum << 1) > len)
+		return true;
+	return false;
+}
+
+int MoreThanHalfNum(int* numbers, int len)
+{
+    if (CheckInvalidArray(numbers, len))
+		return 0;
+	int middle = len >> 1;
+	int start = 0, end = len - 1, res = 0;
+	int index = Partition(numbers, len, start, end);
+	while (middle != index)
+	{
+		if (index > middle)
+		{
+			end = index - 1;
+			index = Partition(numbers, len, start, end);
+		}
+		else 
+		{
+			start = index + 1;
+			index = Partition(numbers, len, start, end);
+		}
+	}
+	
+	res = numbers[index];
+	if (!CheckMoreThanHalf(numbers, len, res))
+		res = 0;
+	return res;
 }
 
 
 void test1()
 {
-
+	int vec[] = { 3,2,3,3,3,2,4,3,5 };
+	int res = MoreThanHalfNum(vec, sizeof(vec) / sizeof(int));
 }
 
 int main(int argc, char* argv[])
