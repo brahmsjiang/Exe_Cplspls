55a481ed234fa23a0e258533f6e5684e84aebb31
ljiang <ljiang@telenav.cn>
2020-04-21 18:02:23 +0800

    CP-1218, remove additional log

---
+++ sword to offer/39.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int MoreThanHalfNum(int* numbers, int length)
+{
+    if (CheckInvalidArray(numbers, length))
+        return 0;
+
+    int pivot = numbers[0];
+    int i = 0, j = length - 1, res = 0;
+
+    while (i < j)
+    {
+        while (i < j && numbers[j] >= pivot)
+            --j;
+        if (i < j && numbers[j] < pivot)
+        {
+            numbers[i] = numbers[j];
+            ++i;
+        }
+
+        while (i < j && numbers[i] <= pivot)
+            ++i;
+        if (i < j && numbers[i] > pivot)
+        {
+            numbers[j] = numbers[i];
+            --j;
+        }
+    }
+    numbers[i] = pivot;
+    if (i == length / 2)
+        res = numbers[i];
+    else
+    {
+        res = MoreThanHalfNum() || MoreThanHalfNum();
+    }
+    return res;
+
+}
+
+
+void test1()
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    test1();
+
+    return 0;
+}
