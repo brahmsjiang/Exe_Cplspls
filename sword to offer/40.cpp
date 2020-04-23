7e025ba135a8a1d27c2b482c087ac770fd92969e
ljiang <ljiang@telenav.cn>
2020-04-23 18:06:16 +0800

    AUTOREF-13510, use waypoints turntype to replace cursegs turntype

---
+++ sword to offer/40.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int Partition(int* numbers, int len, int start, int end)
+{
+    if (numbers == nullptr || len <= 0 || len <= (end - start))
+        return -1;
+
+    int pivot = numbers[0], i = start, j = end;
+    while (i < j)
+    {
+
+    }
+}
+
+void GetleastNumbers(int* input, int n, int* output, int k)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
