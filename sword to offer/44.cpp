542f0c2e77ccba57f751cda970d24ed5050123d5
brahmsjiang <brahmsjiang@163.com>
2020-05-14 23:00:21 +0800

    update

--- sword to offer/44.cpp
+++ sword to offer/44.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-int digitAtIndex(int index)
+int digitAtIndex_1(int index)
 {
     int totalIdx = 0, curIdx = 0;
     unsigned int curNum = 0, curDigit = 0;
@@ -20,17 +20,22 @@ int digitAtIndex(int index)
         }
         totalIdx += curIdx;
     }
-    totalIdx = 
+	//now totalIdx >= idx, the target digit should be found from curNum, whose num of digit is curIdx
+	int tarIdx = totalIdx - index;//tarIdx is from right to left, if curNum is "132", par index point to "3", tarIdx should be 1
+	cout << "curNum is:" << curNum << " curIdx is:" << curIdx << " totalIdx is:" << totalIdx <<  endl;
+	do
+	{
+		curDigit = curNum % 10;
+		curNum /= 10;
+		--tarIdx;
+	} while (tarIdx >= 0);
 
-    while (totalIdx++ != index)
-    {
-        curNum
-    }
     return curDigit;
 }
 
 
 int main(int argc, char* argv[])
 {
+	int res = digitAtIndex_1(15);
     return 0;
 }
