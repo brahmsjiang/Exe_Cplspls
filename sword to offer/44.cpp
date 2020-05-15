b504527d36d9986ba600260f819ac79f201acaba
ljiang <ljiang@telenav.cn>
2020-05-15 17:30:25 +0800

    AUTOREF-14406, integrate navcore rc13

--- sword to offer/44.cpp
+++ sword to offer/44.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int digitAtIndex_1(int index)
 {
+    if (index < 0)  //don't forget it
+        return -1;
+
     int totalIdx = 0, curIdx = 0;
     unsigned int curNum = 0, curDigit = 0;
     while (totalIdx < index)
@@ -33,9 +36,38 @@ int digitAtIndex_1(int index)
     return curDigit;
 }
 
+//first digit of m-bit num
+int beginNumber(int digits)
+{
+    if (digits == 1)
+        return 0;
+    
+}
+
+//how many nums included in m digits. m=3, thus nums is 90(10~99)
+int countOfIntegers(int digits)
+{
+
+}
+
+int digitAtIndex_2(int index, int digits)
+{
+    if (index < 0)
+        return -1;
+
+}
+
+int digitAtIndex_2(int index)
+{
+    if (index < 0)
+        return -1;
+
+}
+
 
 int main(int argc, char* argv[])
 {
 	int res = digitAtIndex_1(15);
+    res = digitAtIndex_2(15);
     return 0;
 }
