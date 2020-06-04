4f0313943087d01bfc163d3efac3ace3abcd434b
brahmsjiang <brahmsjiang@163.com>
2020-06-04 23:50:39 +0800

    update

--- sword to offer/45.cpp
+++ sword to offer/45.cpp
@@ -7,48 +7,69 @@ using namespace std;
 
 int layer = 0;
 bool layprinted = false;
+
+int printNum(int* nums, int len)
+{
+	std::string res;
+	for (size_t i = 0; i < len; i++)
+	{
+		res += std::to_string(nums[i]);
+	}
+	return atoi(res.c_str());
+}
+
+void printTab(int layer)
+{
+	if (layer > 0)
+	{
+		for (size_t i = 0; i < layer; i++)
+			printf("    ");
+	}
+}
+
 void curVal(int* nums, int pBegin, int len, int& maxVal)
 {
-    if (layer > 0)
-    {
-        for (size_t i = 0; i < layer; i++)
-            printf("    ");
-    }
+	printTab(layer);
     ++layer;
 
-    std::string res;
-    for (size_t i = 0; i < len; i++)
-    {
-        res += std::to_string(nums[i]);
-    }
-    int newMaxval = atoi(res.c_str());
-
-    printf("pBegin: %d  nums: %d    ", pBegin, newMaxval);
+	int curNum = printNum(nums, len);
+    printf("==>pBegin: %d  nums: %d\n", pBegin, curNum);
     if (pBegin == len)
     {
-        printf("==>maxVal: %d", newMaxval);
-        maxVal = (newMaxval > maxVal) ? newMaxval : maxVal;
+		printTab(layer);
+        printf("=>=>maxVal: %d\n", curNum);
+        maxVal = (curNum > maxVal) ? curNum : maxVal;
     }
     else 
     {
         for (size_t pCur = pBegin; pCur < len; pCur++)
         {
-            printf("pBegin[%d] pCur[%d]  in loop\n", pBegin, pCur);
+			printTab(layer);
+            printf("||start thisloop, nums: %d pBegin[%d] pCur[%d]  in loop\n", printNum(nums, len), pBegin, pCur);
+
             //exchange head's element with mid's element
             int tmp = nums[pCur];
             nums[pCur] = nums[pBegin];
             nums[pBegin] = tmp;
 
+			printTab(layer);
+			printf("|before curVal, nums: %d\n", printNum(nums, len));
+
             curVal(nums, pCur + 1, len, maxVal);
+
+			printTab(layer);
+			printf("|after curVal, nums: %d\n", printNum(nums, len));
 
             tmp = nums[pCur];
             nums[pCur] = nums[pBegin];
             nums[pBegin] = tmp;
+
+			printTab(layer);
+			printf("|end thisloop, nums: %d\n", printNum(nums, len));
         }
     }
     
-    cout << endl;
-    layprinted = false;
+	--layer;
 }
 
 void PrintMinNum1(int* nums, int len)
