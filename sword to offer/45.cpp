fd310ceb6bf7f00ef91d5fc2e531356f726386bf
ljiang <ljiang@telenav.cn>
2020-06-05 11:13:04 +0800

    AUTOREF-14540, modify log for test

--- sword to offer/45.cpp
+++ sword to offer/45.cpp
@@ -1,39 +1,33 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include "stdio.h"
-
 
 using namespace std;
 
 int layer = 0;
-bool layprinted = false;
 
 int printNum(int* nums, int len)
 {
 	std::string res;
 	for (size_t i = 0; i < len; i++)
-	{
 		res += std::to_string(nums[i]);
-	}
-	return atoi(res.c_str());
+
+    return atoi(res.c_str());
 }
 
 void printTab(int layer)
 {
-	if (layer > 0)
-	{
-		for (size_t i = 0; i < layer; i++)
-			printf("    ");
-	}
+    for (size_t i = 0; i < layer; i++)
+        printf("    ");
 }
 
-void curVal(int* nums, int pBegin, int len, int& maxVal)
+void permutation(int* nums, int pBegin, int len, int& maxVal)
 {
 	printTab(layer);
     ++layer;
 
 	int curNum = printNum(nums, len);
-    printf("==>pBegin: %d  nums: %d\n", pBegin, curNum);
+    printf("==>enter curVal, permu_head: %d  nums: %d\n", pBegin, curNum);
     if (pBegin == len)
     {
 		printTab(layer);
@@ -45,7 +39,7 @@ void curVal(int* nums, int pBegin, int len, int& maxVal)
         for (size_t pCur = pBegin; pCur < len; pCur++)
         {
 			printTab(layer);
-            printf("||start thisloop, nums: %d pBegin[%d] pCur[%d]  in loop\n", printNum(nums, len), pBegin, pCur);
+            printf("||start thisloop, nums: %d permu_head[%d] exchg_idx[%d]  in loop\n", printNum(nums, len), pBegin, pCur);
 
             //exchange head's element with mid's element
             int tmp = nums[pCur];
@@ -55,14 +49,7 @@ void curVal(int* nums, int pBegin, int len, int& maxVal)
 			printTab(layer);
 			printf("|before curVal, nums: %d\n", printNum(nums, len));
 
-            curVal(nums, pCur + 1, len, maxVal);
-
-			printTab(layer);
-			printf("|after curVal, nums: %d\n", printNum(nums, len));
-
-            tmp = nums[pCur];
-            nums[pCur] = nums[pBegin];
-            nums[pBegin] = tmp;
+            permutation(nums, pBegin + 1, len, maxVal);
 
 			printTab(layer);
 			printf("|end thisloop, nums: %d\n", printNum(nums, len));
@@ -79,19 +66,25 @@ void PrintMinNum1(int* nums, int len)
         return;
 
     int maxVal = 0;
-    curVal(nums, 0, len, maxVal);
+    permutation(nums, 0, len, maxVal);
 
-    //cout << maxVal << endl;
+    cout << "[maxVal]====>" << maxVal << endl;
 }
 
 void PrintMinNum2(int* nums, int len)
 {
+    using namespace std;
+    if (nums == nullptr)
+        return;
+    int maxVal = 0;
+    
 
+    cout << "[maxVal]====>" << maxVal << endl;
 }
 
 int main(int argc, char* argv[])
 {
-    int vec1[] = { 4, 3, 5 };
+    int vec1[] = { 4, 3, 5, 1 };
     PrintMinNum1(vec1, sizeof(vec1)/sizeof(int));
     return 0;
 }
