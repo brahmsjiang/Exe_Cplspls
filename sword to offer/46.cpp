78fdff3c28cdceb18920f072d518c4a6233e0695
ljiang <ljiang@telenav.cn>
2020-06-12 16:56:11 +0800

    WK-34, integrate font provided by navcore

--- sword to offer/46.cpp
+++ sword to offer/46.cpp
@@ -4,30 +4,78 @@
 
 using namespace std;
 
-int getTransCount(int number)
+int layer = 0;
+void printTab(int layer)
 {
-    if (number < 0)
-        return 0;
-    
-    string numberInStr = std::to_string(number);
-    return getTransCount(numberInStr);
+    for (size_t i = 0; i < layer; i++)
+        printf("    ");
 }
 
 int getTransCount(const string& number)
 {
-    int length = number.length();
-    int* counts = new int[length];
-    int count = 0;
+    ++layer;
+    printTab(layer);
 
-    for (int i = length - 1; i >= 0; --i)
+    cout << "==>cur number:" << number << endl;
+
+    if (number.empty())
     {
-
+        printTab(layer);
+        cout << "count: 1" << endl;
+        --layer;
+        return 1;
     }
 
+    int count = 0;
+    char curNumberstr[3];
+    memset(curNumberstr, 0, sizeof(curNumberstr) / sizeof(char));
+
+    //last char
+    string numbertmp = number;
+    numbertmp.pop_back();
+    printTab(layer);
+    cout << "before getTransCount, use cur char1" << endl;
+    count = getTransCount(numbertmp);
+
+    //second last two char
+    if (number.size() >= 2)
+    {
+        strcat(curNumberstr, number.substr(number.size() - 2, 2).c_str());
+        if (10 <= atoi(curNumberstr) && atoi(curNumberstr) <= 25)
+        {
+            numbertmp.pop_back();
+            printTab(layer);
+            cout << "before getTransCount, use cur char2" << endl;
+            count += getTransCount(numbertmp);
+        }
+    }
+   
+    printTab(layer);
+    cout << "count: " << count << endl;
+
+    --layer;
+    return count;
+}
+
+int getTransCount(int number)
+{
+    if (number < 0)
+        return 0;
+
+    string numberInStr = std::to_string(number);
+    return getTransCount(numberInStr);
 }
 
 int main(int argc, char* argv[])
 {
-
+    cout << getTransCount(0) << endl;
+    cout << getTransCount(10) << endl;
+    cout << getTransCount(125) << endl;
+    cout << getTransCount(126) << endl;
+    cout << getTransCount(426) << endl;
+    cout << getTransCount(100) << endl;
+    cout << getTransCount(101) << endl;
+    cout << getTransCount(12258) << endl;
+    cout << getTransCount(-100) << endl;
     return 0;
 }
