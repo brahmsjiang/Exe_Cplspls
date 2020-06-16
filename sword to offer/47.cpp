708f6ade8557a340b9a23ea3036a0f8eb2de4786
ljiang <ljiang@telenav.cn>
2020-06-16 16:14:35 +0800

    AUTOREF-14489, delete redundant JNI logs

--- sword to offer/47.cpp
+++ sword to offer/47.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,22 +13,64 @@ int getMaxValue_sol1(const int* values, int rows, int cols)
         maxValues[i] = new int[cols];
     }
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            maxValues[i][j] = *(values + i*cols + cols);
-            int lastTraceVal = 0;
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            int left = 0;
+            int up = 0;
+            
             if (i > 0)
-                lastTraceVal = maxValues[i - 1][j];
-            if (j > 0 && maxValues[i][j - 1] > lastTraceVal)
-                lastTraceVal = maxValues[i][j - 1];
-
-            maxValues[i][j] += lastTraceVal;
+                up = maxValues[i - 1][j];
+            if (j > 0)
+                left = maxValues[i][j - 1];
+            maxValues[i][j] = std::max(left, up) + values[i * cols + j];
         }
     }
-    return maxValues[rows - 1][cols - 1];
+
+    int res = maxValues[rows - 1][cols - 1];
+
+    for (int i = 0; i < rows; ++i)
+        delete[] maxValues[i];
+    delete[] maxValues;
+
+    return res;
 }
+
+int getMaxValue_sol2(const int* values, int rows, int cols)
+{
+    if (values == nullptr || rows <= 0 || cols <= 0)
+        return 0;
+
+    int* maxValues = new int[cols];
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            int left = 0;
+            int up = 0;
+
+            if (i > 0)
+                up = maxValues[j];
+            if (j > 0)
+                left = maxValues[j - 1];
+
+            maxValues[j] = std::max(left, up) + values[i * cols + j];
+        }
+    }
+
+    int res = maxValues[cols - 1];
+
+    delete[] maxValues;
+
+    return res;
+}
+
 
 int main(int argc, char* argv[])
 {
+    int vec1[] = { 1,10,3,8,12,2,9,6,5,7,4,11,3,7,16,5 };
+    int res = getMaxValue_sol1(vec1, 4, 4);
+    res = getMaxValue_sol2(vec1, 4, 4);
     return 0;
 }
