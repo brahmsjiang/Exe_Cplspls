58181da9c3491389a9f1b5906525ab6c2e600ec6
brahmsjiang <brahmsjiang@163.com>
2020-07-08 23:05:05 +0800

    update

--- sword to offer/49.cpp
+++ sword to offer/49.cpp
@@ -33,6 +33,13 @@ int GetUglyNumber(int index)
     return number;
 }
 
+int NxtUgly(int t2, int t3, int t5)
+{
+	int minVal = (t2 < t3) ? t2 : t3;
+	minVal = (minVal < t5) ? minVal : t5;
+	return minVal;
+}
+
 int GetUglyNumber2(int index)
 {
 	if (index <= 0)
@@ -40,29 +47,53 @@ int GetUglyNumber2(int index)
 
 	int* uglyNums = new int[index];
 	int T2, T3, T5;
-	int curIdx = 0;
+
+	uglyNums[0] = 1;
+	T2 = T3 = T5 = 1;
+	int curIdx = 1;//idx start from 1
 
 	while (curIdx < index)
 	{
-		if (curIdx == 0)
-		{
-			uglyNums[curIdx] = 1;
-			T2 = T3 = T5 = 1;
+		uglyNums[curIdx] = NxtUgly(T2 * 2, T3 * 3, T5 * 5);
+		for (size_t i = 0; i <= curIdx; ++i) {
+			if (uglyNums[i] >= T2 && uglyNums[i] * 2 > uglyNums[curIdx]) {
+				T2 = uglyNums[i];
+				break;
+			}
 		}
+
+		for (size_t i = 0; i <= curIdx; ++i) {
+			if (uglyNums[i] >= T3 && uglyNums[i] * 3 > uglyNums[curIdx]) {
+				T3 = uglyNums[i];
+				break;
+			}
+		}
+
+		for (size_t i = 0; i <= curIdx; ++i) {
+			if (uglyNums[i] >= T5 && uglyNums[i] * 5 > uglyNums[curIdx]) {
+				T5 = uglyNums[i];
+				break;
+			}
+		}
+
 		curIdx++;
 	}
-	assert(curIdx == index);
 
-
+	int res = uglyNums[curIdx - 1];
+	delete[] uglyNums;
+	return res;
 }
 
 int main(int argc, char* argv[])
 {
     clock_t t0 = clock();
-    auto res = GetUglyNumber(1500);
+    auto res = GetUglyNumber(150);
     clock_t t1 = clock();
     std::cout << static_cast<double>(t1 - t0) << "ms"<< std::endl;
 
-    auto res2 = GetUglyNumber2(1500);
+	t0 = clock();
+    auto res2 = GetUglyNumber2(150);
+	t1 = clock();
+	std::cout << static_cast<double>(t1 - t0) << "ms" << std::endl;
     return 0;
 }
