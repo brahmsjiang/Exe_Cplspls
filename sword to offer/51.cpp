887828e495660bb9e5662af569488ad04d18d2e9
ljiang <ljiang@telenav.cn>
2020-07-16 15:55:44 +0800

    WK-369, update resource.db for HW shield icon

--- sword to offer/51.cpp
+++ sword to offer/51.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <set>
+
+int InversePairs_1(int* data, int length)
+{
+    if (data == nullptr || length < 0)
+        return 0;
+
+    int NumOfInverse = 0;
+    for (int i = 0; i < length; ++i)
+    {
+        for (int j = i; j < length; ++j)
+        {
+            if (data[i] > data[j])
+                NumOfInverse++;
+        }
+    }
+    return NumOfInverse;
+}
+
+int InversePairs_2(int* data, int length)
+{
+    if (data == nullptr || length < 0)
+        return 0;
+
+    return 0;
+}
 
 int main(int argc, char* argv[])
 {
+    int data[] = { 7,5,6,4 };
+    auto res1 = InversePairs_1(data, sizeof(data)/sizeof(int));
     return 0;
 }
