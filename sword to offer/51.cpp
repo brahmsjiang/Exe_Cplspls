82506d1ac88cd54579f19e239299bf0df1593e0c
brahmsjiang <brahmsjiang@163.com>
2020-07-23 11:20:37 +0800

    update

--- sword to offer/51.cpp
+++ sword to offer/51.cpp
@@ -24,30 +24,54 @@ int InversePairs_1(int* data, int length)
 
 void merge(int* data, int low, int mid, int high, int& inverNum)
 {
-    int i = low;
-    int j = mid + 1;
-    int idx_tmp = 0;
-    int* tmpdata = new int[high - low + 1];
-    while (i <= mid && j <= high)
+    int i = mid;
+    int j = high;
+    int idx_tmp = high - low;
+	const int len_tmp = high - low + 1;
+    int* tmpdata = new int[len_tmp];
+    while (i >= low && j >= mid + 1)
     {
         if (data[i] > data[j])
         {
-            printf("[[%d, %d]]\n", data[i], data[j]);
-            tmpdata[idx_tmp++] = data[j++];
-            inverNum++;
+			int idx = j;
+			while (idx >= mid + 1)
+			{
+				printf("(data[%d]:%d, data[%d]:%d)\n", i, data[i], idx, data[idx]);
+				--idx;
+			}
+            
+			inverNum += (j - mid);
+			tmpdata[idx_tmp--] = data[i--];
         }
         else
         {
-            tmpdata[idx_tmp++] = data[i++];
+            tmpdata[idx_tmp--] = data[j--];
         }
     }
-    while (i > mid && j <= high)
-        tmpdata[idx_tmp++] = data[j++];
-    while (i <= mid && j > high)
-        tmpdata[idx_tmp++] = data[i++];
+	while (i < low && j >= mid + 1)
+	{
+		tmpdata[idx_tmp--] = data[j--];
+	}
+	while (i >= low && j < mid + 1)
+	{
+		tmpdata[idx_tmp--] = data[i--];
+	}
 
-    memcpy(data + low, tmpdata, sizeof(tmpdata));
-    delete[] tmpdata;
+	for (size_t i = 0; i < len_tmp; i++)
+	{
+		printf("tmpdata[%d]:%d ", i, tmpdata[i]);
+	}
+	std::cout << std::endl;
+
+    memcpy(data + low, tmpdata, sizeof(int) * len_tmp);
+	for (size_t i = low; i <= high; i++)
+	{
+		printf("data[%d]:%d ", i, data[i]);
+	}
+	std::cout << std::endl;
+
+	delete[] tmpdata;
+	tmpdata = nullptr;
 }
 
 void merge_sort(int* data, int low, int high, int& inverNum)
@@ -75,12 +99,9 @@ int InversePairs_2(int* data, int length)
 }
 
 
-
-
-
 int main(int argc, char* argv[])
 {
-    int data[] = { 7,5,6,4 };
+    int data[] = { 7,5,3,6,4 };
     auto res1 = InversePairs_1(data, sizeof(data) / sizeof(int));
     auto res2 = InversePairs_2(data, sizeof(data) / sizeof(int));
     return 0;
