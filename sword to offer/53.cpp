94568dcefcc41ba217694682f1df4d67368c8893
ljiang <ljiang@telenav.cn>
2020-07-30 18:00:59 +0800

    WK-638, add rockfall for dangerous event warning

--- sword to offer/53.cpp
+++ sword to offer/53.cpp
@@ -2,6 +2,28 @@
 #include <stack>
 #include <math.h>
 
+int GetFirstK(int* data, int length, int k, int start, int end)
+{
+    if (start > end)
+        return -1;
+    int mid = (start + end) / 2;
+
+}
+
+int GetLastK(int* data, int length, int k, int start, int end)
+{
+
+}
+
+int GetNumberOfK(int* data, int length, int k)
+{
+    if (data == nullptr || 0 >= length)
+        return -1;
+    int midIdx = (length - 1) / 2;
+    int firstKIdx = GetFirstK(data, length, k, 0, midIdx);
+    int LastKIdx = GetLastK(data, length, k, midIdx + 1, length - 1);
+
+}
 
 int main(int argc, char* argv[])
 {
