c6ebbb92eaeb9d43faa1c652422f99b199bc8998
ljiang <ljiang@telenav.cn>
2020-07-31 16:08:53 +0800

    WK-638, add rockfall for dangerous event warning

--- sword to offer/53.cpp
+++ sword to offer/53.cpp
@@ -2,31 +2,70 @@
 #include <stack>
 #include <math.h>
 
-int GetFirstK(int* data, int length, int k, int start, int end)
+int GetFirstK(int* data, int k, int start, int end)
 {
     if (start > end)
         return -1;
+
     int mid = (start + end) / 2;
+    int midData = data[mid];
+    if (midData == k)
+    {
+        if (mid > 0 && data[mid - 1] != k || mid == 0)
+            return mid;
+        else
+            end = mid - 1;
+    }
+    else if (midData > k)
+        end = mid - 1;
+    else
+        start = mid + 1;
+
+    return GetFirstK(data, k, start, end);
 
 }
 
-int GetLastK(int* data, int length, int k, int start, int end)
+int GetLastK(int* data, int k, int start, int end)
 {
+    if (start > end)
+        return -1;
+
+    int mid = (start + end) / 2;
+    int midData = data[mid];
+    if (midData == k)
+    {
+        if (mid < end && data[mid + 1] != k || mid == end)
+            return mid;
+        else
+            start = mid + 1;
+    }
+    else if (midData < k)
+        start = mid + 1;
+    else
+        end = mid - 1;
+
+    return GetLastK(data, k, start, end);
 
 }
 
 int GetNumberOfK(int* data, int length, int k)
 {
-    if (data == nullptr || 0 >= length)
-        return -1;
-    int midIdx = (length - 1) / 2;
-    int firstKIdx = GetFirstK(data, length, k, 0, midIdx);
-    int LastKIdx = GetLastK(data, length, k, midIdx + 1, length - 1);
+    int number = 0;
+    if (data != nullptr || 0 < length)
+    {
+        int first = GetFirstK(data, k, 0, length - 1);
+        int last = GetLastK(data, k, 0, length - 1);
 
+        if (first > -1 && last > -1)
+            number = last - first + 1;
+    }
+
+    return number;
 }
 
 int main(int argc, char* argv[])
 {
-
+    int vec[] = { 1,1,2,3,3,3,3,4,4 };
+    auto res1 = GetNumberOfK(vec, sizeof(vec) / sizeof(int), 4);
     return 0;
 }
---
+++ sword to offer/54.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stack>
+#include <math.h>
+
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
