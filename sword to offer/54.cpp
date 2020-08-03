70e1dea0075cd1849e78df8ccd570ed9e81f73e7
ljiang <ljiang@telenav.cn>
2020-08-03 17:52:53 +0800

    WK-638, add rockfall for dangerous event warning

--- sword to offer/54.cpp
+++ sword to offer/54.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <stack>
-#include <math.h>
+#include "Utilities/BinaryTree.h"
 
+
+BinaryTreeNode* findKNode(const BinaryTreeNode* root, int k)
+{
+    if (root == nullptr)
+        return nullptr;
+
+    BinaryTreeNode* res = nullptr;
+    int curCount = 0;
+
+    res = 
+
+}
 
 int main(int argc, char* argv[])
 {
+    BinaryTreeNode* root = CreateBinaryTreeNode(5);
+    BinaryTreeNode* node3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* node7 = CreateBinaryTreeNode(7);
+    BinaryTreeNode* node2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* node4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* node6 = CreateBinaryTreeNode(6);
+    BinaryTreeNode* node8 = CreateBinaryTreeNode(8);
+    ConnectTreeNodes(root, node3, node7);
+    ConnectTreeNodes(node3, node2, node4);
+    ConnectTreeNodes(node7, node6, node8);
+
+    auto res = findKNode(root, 3);
 
     return 0;
 }
