3d47c3d0492e0e8cfd2431d42eb57579a9247bce
ljiang <ljiang@telenav.cn>
2020-08-21 19:49:15 +0800

    WK-755, fix prompt eng strname when langcode is thai

--- sword to offer/55.cpp
+++ sword to offer/55.cpp
@@ -35,13 +35,13 @@ bool IsBalanced2(BinaryTreeNode* pRoot, int& depth)
     }
 
     bool ret = false;
-    int leftDepth, rightDepth;
+    int leftDepth = 0, rightDepth = 0;
     if (IsBalanced2(pRoot->m_pLeft, leftDepth) && IsBalanced2(pRoot->m_pRight, rightDepth))
     {
         ret = abs(leftDepth - rightDepth) <= 1;
     }
     
-    depth = 1 + (leftDepth > rightDepth) ? leftDepth : rightDepth;
+    depth = 1 + ((leftDepth > rightDepth) ? leftDepth : rightDepth);
     printf("cur node: %d, leftDepth: %d, rightDepth: %d, depth: %d\n", pRoot->m_nValue, leftDepth, rightDepth, depth);
     return ret;
 }
@@ -57,11 +57,13 @@ int main(int argc, char* argv[])
     BinaryTreeNode* node6 = CreateBinaryTreeNode(6);
     BinaryTreeNode* node7 = CreateBinaryTreeNode(7);
     BinaryTreeNode* node8 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* node9 = CreateBinaryTreeNode(9);
     ConnectTreeNodes(root, node2, node3);
     ConnectTreeNodes(node2, node4, node5);
     ConnectTreeNodes(node5, node7, nullptr);
     //ConnectTreeNodes(node7, node8, nullptr);
     ConnectTreeNodes(node3, nullptr, node6);
+    //ConnectTreeNodes(node6, nullptr, node9);
 
     auto res = IsBalanced(root);
 
