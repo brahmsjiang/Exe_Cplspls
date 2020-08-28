b02a00e3ed9d7c9b494eb79b463c87e704b2998a
brahmsjiang <brahmsjiang@163.com>
2020-08-28 23:21:26 +0800

    update

--- sword to offer/57.cpp
+++ sword to offer/57.cpp
@@ -28,11 +28,51 @@ bool FindNumbersWithSum(int data[], int length, int sum, int& num1, int& num2)
     return found;
 }
 
+void PrintContinousSequence(int smlIdx, int largIdx)
+{
+	using namespace std;
+	cout << "(";
+	while (smlIdx <= largIdx)
+	{
+		cout << smlIdx;
+		if(smlIdx != largIdx)
+			cout << "+";
+		++smlIdx;
+	}
+	cout << ")" << endl;
+}
+
+void FindContinousSequence(int sum)
+{
+	if (sum < 3)
+		return;
+
+	int smlIdx = 1, largIdx = 2;
+	int mid = (1 + sum) / 2;
+	while (smlIdx < mid)
+	{
+		int curSum = (smlIdx + largIdx) * (largIdx - smlIdx + 1) / 2;
+		if (curSum == sum)
+		{
+			PrintContinousSequence(smlIdx, largIdx);
+			++smlIdx;
+			largIdx = smlIdx + 1;
+		}
+		else if (curSum < sum)
+			++largIdx;
+		else
+			++smlIdx;
+	}
+}
+
 int main(int argc, char* argv[])
 {
     int vec1[] = { 1,2,4,7,11,15 };
     int num1 = 0, num2 = 0;
     auto res1 = FindNumbersWithSum(vec1, sizeof(vec1) / sizeof(int), 15, num1, num2);
 
+	FindContinousSequence(9);
+	std::cout << "--------" << std::endl;
+	FindContinousSequence(27);
     return 0;
 }
