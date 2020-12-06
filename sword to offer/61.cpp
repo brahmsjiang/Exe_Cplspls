0820e49359898a188df6d340e8225cd518a8900f
brahmsjiang <brahmsjiang@163.com>
2020-12-06 22:00:02 +0800

    update

---
+++ sword to offer/61.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int compare(const void* arg1, const void* arg2)
+{
+	return *(int*)arg1 - *(int*)arg2;
+}
+
+bool isContinuous(int* numbers, int len)
+{
+	if (nullptr == numbers || len < 1)
+		return false;
+
+	qsort(numbers, len, sizeof(int), compare); //qsort, from stdlib.h
+	
+	int numZero = 0;
+	int numGap = 0;
+	
+	int lstVal = numbers[0], curVal = 0;
+	bool invalid = false;
+	for (size_t i = 0; i < len; i++)
+	{
+		if (numbers[i] == 0)
+			++numZero;
+
+		if (i > 0)
+		{
+			curVal = numbers[i];
+			if (curVal != 0)
+			{
+				if (curVal == lstVal)
+					return false;
+				if (curVal != lstVal + 1)
+					numGap = (curVal - lstVal - 1);
+			}
+			lstVal = curVal;
+		}
+	}
+	return !numGap || (numGap == numZero);
+}
+
+int main(int argc, char* argv[])
+{
+	int vec[5] = { 5,3,0,1,0 };
+	auto res = isContinuous(vec, 5);
+	return 0;
+}
