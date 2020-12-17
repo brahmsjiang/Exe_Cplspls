7bbd68226ec881c1df5534855252622b76cf4191
ljiang <ljiang@telenav.cn>
2020-12-17 22:24:14 +0800

    remove build win64 in jenkins

---
+++ API design for C++/3.4 wrapper.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class IOriginal
+{
+public:
+    virtual bool DoSomething(int value) = 0;
+};
+
+class Original : public IOriginal
+{
+public:
+    bool DoSomething(int value) {}
+private:
+
+};
+
+class Proxy : public IOriginal
+{
+public:
+    
+private:
+    Proxy(const Proxy&);
+    const Proxy& operator=(const Proxy&);
+
+    Original* mOrig;
+};
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
