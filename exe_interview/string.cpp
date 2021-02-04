f989ce056ce97bf38bf66799defc9187097e1037
ljiang <ljiang@telenav.cn>
2021-02-04 23:15:11 +0800

    NZ-1206, use the 3 timeout for different range routes

--- exe_interview/string.cpp
+++ exe_interview/string.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     string ostr("GET / HTTP / 1.1");
-    size_t ops = ostr.find("/");
+    size_t ops = ostr.find("/", 0);
     cout << ops << endl;
-    ops = ostr.find("/");
+    ops = ostr.find("/", ops+1);
     cout << ops << endl;
     return 0;
 }
