c65cf61fa06634189d37c692d52fbc66b8caf607
ljiang <ljiang@telenav.cn>
2021-03-08 17:59:58 +0800

    WK-2468, add highway icon again for SEA

--- C++11/iterator_newfor_geek/Source.cpp
+++ C++11/iterator_newfor_geek/Source.cpp
@@ -1,3 +1,5 @@
+#include "istream_line_reader.h"
+
 #include <algorithm>
 #include <iterator>
 #include <vector>
@@ -6,80 +8,7 @@
 
 using namespace std;
 
-class istream_line_reader
-{
-public:
-    class iterator
-    {
-    public:
-        typedef ptrdiff_t difference_type;
-        typedef string value_type;
-        typedef const value_type* pointer;
-        typedef const value_type& reference;
-        typedef input_iterator_tag iterator_category;
 
-        iterator() noexcept
-            : stream_(nullptr) {}
-
-        explicit iterator(istream& is)
-            : stream_(&is)
-        {
-            ++*this; //operator priority from right to left, '++' and '*' seems the same
-        }
-
-        reference operator*() const noexcept
-        {
-            return line_;
-        }
-        pointer operator->() const noexcept
-        {
-            return &line_;
-        }
-        iterator& operator++()
-        {
-            getline(*stream_, line_);
-            if (!*stream_) {
-                stream_ = nullptr;
-            }
-            return *this;
-        }
-        iterator operator++(int)
-        {
-            iterator temp(*this);
-            ++*this;
-            return temp;
-        }
-        bool operator==(const iterator& rhs) const noexcept
-        {
-            return stream_ == rhs.stream_;
-        }
-        bool operator!=(const iterator& rhs) const noexcept
-        {
-            return !operator==(rhs);
-        }
-
-    private:
-        istream* stream_;
-        string line_;
-    };
-
-    istream_line_reader() noexcept
-        : stream_(nullptr) {}
-    explicit istream_line_reader(istream& is) noexcept
-        : stream_(&is) {}
-
-    iterator begin()
-    {
-        return iterator(*stream_);
-    }
-    iterator end() const noexcept
-    {
-        return iterator();
-    }
-
-private:
-    istream* stream_;
-};
 
 int main(int argc, char* argv[])
 {
