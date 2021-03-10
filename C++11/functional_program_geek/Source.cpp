951bbcbe187b4779fd81a54e2349b25c5a5792d1
ljiang <ljiang@telenav.cn>
2021-03-10 17:55:09 +0800

    integrate ngx-rc21

--- C++11/functional_program_geek/Source.cpp
+++ C++11/functional_program_geek/Source.cpp
@@ -1,6 +1,11 @@
+#include "istream_line_reader.h"
+
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <numeric> //accumulate 
 
 using namespace std;
 
@@ -30,9 +35,32 @@ int count_lines(const char** begin, const char** end)
     return count;
 }
 
+int count_file_transform(const char* name)
+{
+    ifstream ifs(name);
+    istream_line_reader reader(ifs);
+    return distance(reader.begin(), reader.end());
+
+    /*
+    int count = 0;
+    for (auto&& line : istream_line_reader(ifs)) {
+        ++count;
+    }
+    return count;
+    */
+}
+
+int count_lines_transform(const char** begin, const char** end)
+{
+    vector<int> count(end - begin);//initialize with (end - begin) elements
+
+    transform(begin, end, count.begin(), count_file_transform);
+    return accumulate(count.begin(), count.end(), 0);//return accumulate val from begin to end, based on the initial val of the sum
+}
+
 int main(int argc, const char** argv)
 {
-    int total_lines = count_lines(argv + 1, argv + argc);
+    int total_lines = count_lines_transform(argv + 1, argv + argc);
     cout << total_lines << endl;
 
     return 0;
