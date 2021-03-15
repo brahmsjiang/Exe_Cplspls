4e1d1fb2045ba65c77cb4390b3fc2ed7ac2f48a4
brahmsjiang <brahmsjiang@163.com>
2021-03-15 22:51:13 +0800

    update

--- C++11/container_I_geek/Source.cpp
+++ C++11/container_I_geek/Source.cpp
@@ -27,6 +27,22 @@ void interface_string_test2(const char* str)
     cout << "interface_string_test2: " << str << endl;
 }
 
+class Obj1
+{
+public:
+	Obj1() { cout << "Obj1()\n"; }
+	Obj1(const Obj1&) { cout << "Obj1(const Obj1&)\n"; }
+	Obj1(Obj1&&) noexcept { cout << "Obj1(Obj1&&)\n"; }
+};
+
+class Obj2
+{
+public:
+	Obj2() { cout << "Obj2()\n"; }
+	Obj2(const Obj2&) { cout << "Obj2(const Obj2&)\n"; }
+	Obj2(Obj2&&) { cout << "Obj2(Obj2&&)\n"; }
+};
+
 int main(int argc, char* argv[])
 {
     const char* pchar1 = "constchar*";
@@ -56,9 +72,27 @@ int main(int argc, char* argv[])
     cout << "v1 size: " << v1.size() << " v1 capacity: " << v1.capacity() << "\n";//resize can change, ignore the capacity
     v1.resize(70);
     cout << "v1 size: " << v1.size() << " v1 capacity: " << v1.capacity() << "\n";
-    v1.clear();
     v1.reserve(99);
     cout << "v1 size: " << v1.size() << " v1 capacity: " << v1.capacity() << "\n";//reserve could only increase the capacity
+	v1.pop_back();
+	v1.push_back(69);
+	v1.insert((v1.begin() + 3), 3);//construct a new ele at the iterator specified
+	v1.erase(v1.begin() + 3);//del the ele at the iterator specified
+	v1.emplace((v1.begin() + 3), 3);//result is the same as insert
+	v1.emplace_back(999);
+
+	{
+		vector<Obj1> v11;
+		v11.reserve(1);
+		v11.emplace_back();
+		v11.emplace_back();
+
+		vector<Obj2> v12;
+		v12.reserve(1);
+		v12.emplace_back();//just construct without para
+		v12.emplace_back();
+		//noexcept seems no difference in vs15
+	}
 
 
     list<int> l1{ 1,2,3,4,5 };
