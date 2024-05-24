7e801a58c5fa13d62d25621bc6bed087dd9c7d1a
brahmsjiang <brahmsjiang@163.com>
2024-05-24 17:23:37 +0800

    Update ch3_39.cpp

--- in-depth c++11/in-depth c++11/ch3_39.cpp
+++ in-depth c++11/in-depth c++11/ch3_39.cpp
@@ -9,183 +9,58 @@
 #include <tuple>
 
 using namespace std;
-/*
-namespace detail
+
+template<int...>
+struct IndexTuple{};
+
+template<int N, int... Indexes>
+struct make_indexes : make_indexes<N-1, N-1, Indexes...>{};
+
+//termination condition
+template<int... Indexes>
+struct make_indexes<0, Indexes...> {
+	using type = IndexTuple<Indexes...>;
+};
+
+////////
+template<int I, typename IndexTuple, typename... Types>
+struct make_indexes_reverse_impl;
+
+//declare
+template<int I, int... Indexes, typename T, typename...Types>
+struct make_indexes_reverse_impl<I, IndexTuple<Indexes...>, T, Types...>
 {
-	template<typename T, typename U>
-	typename enable_if<is_convertible<T, U>::value || is_convertible<U, T>::value, bool>::type compare(T t, U u) {
-		return t == u;
-	}
+	using type = typename make_indexes_reverse_impl<I-1, IndexTuple<Indexes..., I-1>, Types...>::type;
+};
 
-	bool compare(...) {
-		return false;
-	}
-
-	template<int I, typename T, typename... Args>
-	struct find_index {
-		static int call(const tuple<Args...>& t, T&& val) {
-			return compare(get<I - 1>(t), val) ? I - 1 : find_index<I - 1, T, Args...>::call(t, forward<T>(val));
-		}
-	};
-
-	template<typename T, typename... Args>
-	struct find_index<0, T, Args...> {
-		static int call(const tuple<Args...>& t, T&& val) {
-			return compare(get<0>(t), val) ? 0 : -1;
-		}
-	};
-}
-
-namespace details
+//terminate
+template<int I, int... Indexes>
+struct make_indexes_reverse_impl<I, IndexTuple<Indexes...>>
 {
-	template<int...>
-	struct IndexTuple{};
-
-	template<int N, int... Indexes>
-	struct make_indexes : make_indexes<N-1, N-1, Indexes...>{};
-
-	//termination condition
-	template<int... Indexes>
-	struct make_indexes<0, Indexes...> {
-		using type = IndexTuple<Indexes...>;
-	};
-
-	template<typename Func, typename Last>
-	void for_each_impl(Func&& f, Last&& last) {
-		f(last);
-	}
-
-	template<typename Func, typename First, typename... Rest>
-	void for_each_impl(Func&& f, First&& first, Rest&&... rest) {
-		f(first);
-		for_each_impl(forward<Func>(f), rest...);
-	}
-
-	template<typename Func, int... Indexes, typename... Args>
-	void for_each_helper(Func&& f, IndexTuple<Indexes...>, tuple<Args...>&& tup) {
-		for_each_impl(forward<Func>(f), forward<Args>(get<Indexes>(tup))...);
-	}
-} // namespace details
-
-template<typename Func, typename Tuple>
-void tp_for_each(Func&& f, Tuple& tup)
-{
-	using namespace details;
-	for_each_helper(forward<Func>(f), typename make_indexes<tuple_size<Tuple>::value>::type(), forward<Tuple>(tup));
-}
-
-template<typename Func, typename... Args>
-void tp_for_each(Func&& f, tuple<Args...>&& tup) {
-	using namespace details;
-	for_each_helper(forward<Func>(f), typename make_indexes<tuple_size<tuple<Args...> >::value>::type(), forward<tuple<Args...> >(tup));
-}
-
-struct Functor {
-	template<typename T>
-	void operator()(T& t) const { cout << t << endl; }
+	using type = IndexTuple<Indexes...>;
 };
 
 
-template<typename T, typename... Args>
-int find_index(tuple<Args...> const& t, T&& val)
-{
-	return detail::find_index<sizeof...(Args), T, Args...>::call(t, forward<T>(val));
+template<typename... Types>
+struct make_reverse_indexes : make_indexes_reverse_impl<sizeof...(Types), IndexTuple<>, Types...>
+{};
+
+template <class... Args, int... Indexes>
+auto reverse_imp(tuple<Args...>&& tup, IndexTuple<Indexes...>&&) {
+	return make_tuple(get<Indexes>(forward<tuple<Args...>>(tup))...);
 }
 
-void TestFindIndex() {
-	tuple<int, double, string> tp = make_tuple(1, 2.1, "ok");
-	int index = find_index(tp, 2.1);
-	cout << index << endl;
+template <class... Args>
+auto Reverse(tuple<Args...>&& tup) {
+	return reverse_imp(forward<tuple<Args...>>(tup), typename make_reverse_indexes<Args...>::type());
 }
 
-template<size_t k, typename Tuple>
-typename enable_if<k == tuple_size<Tuple>::value>::type GetArgByIndex(size_t index, Tuple& tp) {
-	//throw invalid_argument("arg index out of range");
-	cout << ("!!arg index out of range") << endl;
+void TupleReverse() {
+	auto tp2 = Reverse(make_tuple<int, char>(1,'a'));
 }
-
-template<size_t k = 0, typename Tuple>
-typename enable_if<k < tuple_size<Tuple>::value>::type GetArgByIndex(size_t index, Tuple& tp) {
-	if (k == index)
-		cout << get<k>(tp) << endl;
-	else
-		GetArgByIndex<k + 1>(index, tp);
-}
-
-void TestTuple() {
-	using Tuple = tuple<int, double, string, int>;
-	Tuple tp = make_tuple(1, 2, "test", 3);
-	const size_t length = tuple_size<Tuple>::value;
-
-
-	for (size_t i = 0; i < length; ++i) {
-		GetArgByIndex<0>(i, tp);
-	}
-	//GetArgByIndex(4, tp);
-}
-
-template<typename Arg>
-void GetArgByIndex1(int index, tuple<Arg>& tp) {
-	cout << get<0>(tp) << endl;
-}
-
-template<typename T, typename... Args>
-void GetArgByIndex1(int index, tuple<T, Args...>& tp) {
-	if (index < 0 || index >= tuple_size<tuple<T, Args...>>::value)
-		throw invalid_argument("index is not valid");
-	if (index > 0) {
-		Tuple newTp = make_tuple(get<0>(tp)...);
-		GetArgByIndex1(index - 1, tp);
-	}
-	else
-		cout << get<0>(tp) << endl;
-}
-
-void TestTuple1() {
-	using Tuple = tuple<int, double>;
-	Tuple tp = make_tuple(1, 2.1);
-	const size_t length = tuple_size<Tuple>::value;
-
-	for (size_t i = 0; i < length; ++i) {
-	GetArgByIndex<0>(i, tp);
-	}
-
-}
-*/
-
-template<typename T>
-void testFunc(T&& par)
-{
-	cout << par << endl;
-}
-
-template<typename T>
-void testFunc1(tuple<T>&& par)
-{
-	cout << get<0>(par) << endl;
-}
-
 
 int main(int argc, const char * argv[]) {
-	//TestFindIndex();
+	TupleReverse();
 
-	//TestTuple1();
-	testFunc(string("ppt"));
-	string strppt = "ppt";
-	testFunc(strppt);
-	testFunc(move(strppt));
-	///
-	using Tupstr = tuple<string>;
-	Tupstr tupstr = make_tuple<string>("tup_str");
-	testFunc1(make_tuple<string>("tup_str"));
-	//testFunc1(tupstr);//compile err
-	testFunc1(move(tupstr));
-	testFunc1(forward<Tupstr>(tupstr));
-	int lval = 1;
-	int&& rval = 1;
-	//int&& rval1 = lval;//compile err
-	int&& rval1 = forward<int>(lval);
-
-	system("pause");
 	return 0;
 }
