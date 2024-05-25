9def7e7f0eacb5d11df53291f783c49711ebab4d
brahmsjiang <brahmsjiang@163.com>
2024-05-25 17:17:39 +0800

    Update ch3_39.cpp

--- in-depth c++11/in-depth c++11/ch3_39.cpp
+++ in-depth c++11/in-depth c++11/ch3_39.cpp
@@ -14,13 +14,55 @@ template<int...>
 struct IndexTuple{};
 
 template<int N, int... Indexes>
-struct make_indexes : make_indexes<N-1, N-1, Indexes...>{};
+struct MakeIndexes : MakeIndexes<N-1, N-1, Indexes...>{};
 
-//termination condition
 template<int... Indexes>
-struct make_indexes<0, Indexes...> {
-	using type = IndexTuple<Indexes...>;
+struct MakeIndexes<0, Indexes...>
+{
+	typedef IndexTuple<Indexes...> type;
 };
+
+/////
+namespace details
+{
+	template<size_t N, typename T1, typename T2>
+	using pair_type = std::pair<typename tuple_element<N, T1>::type, typename tuple_element<N, T2>::type>;
+
+	template<size_t N, typename T1, typename T2>
+	pair_type<N, T1, T2> pair(const T1& tup1, const T2& tup2) {
+		return make_pair(get<N>(tup1), get<N>(tup2));
+	}
+
+	template<int... Indexes, typename T1, typename T2>
+	auto pairs_helper(IndexTuple<Indexes...>, const T1& tup1, const T2& tup2) {
+		return make_tuple(pair<Indexes>(tup1, tup2)...);
+	}
+}
+
+template<typename Tuple1, typename Tuple2>
+auto Zip(Tuple1 tup1, Tuple2 tup2) {
+	static_assert(tuple_size<Tuple1>::value == tuple_size<Tuple2>::value, "tuples should be the same size.");
+	return details::pairs_helper(typename MakeIndexes<tuple_size<Tuple1>::value>::type(), tup1, tup2);
+}
+
+void TupleZip() {
+	auto tp1 = make_tuple<int, short, double, char>(1, 2, 2.5, 'a');
+	auto tp2 = make_tuple<double, short, double, char>(1.5, 2, 2.5, 'z');
+	auto mypairs = Zip(tp1, tp2);
+	cout << "" << endl;
+}
+
+/////
+template<typename F, typename Tuple, int... Indexes>
+auto apply(F&& f, IndexTuple<Indexes...>&& in, Tuple&& tp) {
+	forward<F>(f)(get<Indexes>(tp)...);
+}
+
+void TestF(int a, double b, int c) { cout << a + b + c << endl;}
+
+void Test() {
+	apply(TestF, MakeIndexes<3>::type(), make_tuple(1, 2.1, 3));
+}
 
 ////////
 template<int I, typename IndexTuple, typename... Types>
@@ -60,7 +102,8 @@ void TupleReverse() {
 }
 
 int main(int argc, const char * argv[]) {
-	TupleReverse();
-
+	//TupleReverse();
+	//Test();
+	TupleZip();
 	return 0;
 }
