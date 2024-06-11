8c22ca7355cf597351dc72b8dc753f49857cb98c
brahmsjiang <brahmsjiang@163.com>
2024-06-11 17:54:33 +0800

    Update ch6_1.cpp

--- in-depth c++11/in-depth c++11/ch6_1.cpp
+++ in-depth c++11/in-depth c++11/ch6_1.cpp
@@ -12,8 +12,26 @@
 using namespace std;
 
 
+template <typename T>
+class testInner
+{};
+
+template <typename T, typename U>
+class testTemp
+{};
+
+
 int main(int argc, const char * argv[]) {
 	
+	std::chrono::milliseconds ms{3};
+	std::chrono::microseconds us = ms;
+	cout << "3 ms has: " << ms.count() << " ticks\n" << "3000 us has: " << us.count() << " ticks\n";
+
+	testTemp<int, char> temp1;
+	testTemp<int, float> temp2;
+	//temp1 = temp2;	//compile err
+
+
 
 
 	return 0;
