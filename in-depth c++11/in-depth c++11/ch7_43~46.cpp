c0eae41978a6c5ccc095e81a4aa12e85b3ff7dcc
brahmsjiang <brahmsjiang@163.com>
2024-06-25 22:02:35 +0800

    Update ch7_43~46.cpp

--- in-depth c++11/in-depth c++11/ch7_43~46.cpp
+++ in-depth c++11/in-depth c++11/ch7_43~46.cpp
@@ -16,6 +16,14 @@ using namespace std;
 
 alignas(32) long long a = 0;
 //alignas(1) long long a1 = 0;	//alignas can't be less than default val
+//alignas(int) long long a1 = 0; //alignas can't be less than default val
+
+#pragma pack(push) //±£´æ¶ÔÆë×´Ì¬
+#pragma pack(1)
+long long a1 = 0;
+#pragma pack(pop)//»Ö¸´¶ÔÆë×´Ì¬
+
+alignas(int) char c = 0;
 
 #define XX 1
 struct alignas(XX)MyStruct_1 {};
@@ -27,11 +35,40 @@ static const unsigned ZZ = 1;//compile phase value
 struct alignas(ZZ)MyStruct_3 {};
 
 
+struct MyStruct {
+	char a;
+	int b;
+	double c;
+};
+
+struct A {
+	int avg;
+	A(int a, int b): avg((a+b)/2) {}
+};
+
+typedef std::aligned_storage<sizeof(A), alignof(A)>::type Aligned_A;
+
 int main(int argc, const char * argv[]) {
 	MyStruct_1 struct1;
 	MyStruct_2<4> struct2;
 	//MyStruct_2<5> struct2;	//err, alignment must be power of two
 	MyStruct_3 struct3;
+
+	//cout << alignof(struct1) << endl; //compile err
+	cout << alignof(MyStruct_1) << endl;
+	cout << alignof(MyStruct_2<4>) << endl;
+	cout << alignof(MyStruct_3) << endl;
+	cout << "<<<<<" << endl;
+
+	int alignsize = alignment_of<MyStruct>::value;//compile phase integral_constant
+	int sz = alignof(MyStruct);
+	cout << alignsize << endl;
+	cout << sz << endl;
+
+	Aligned_A a, b;
+	new (&a) A(10, 20);
+	b = a;
+	cout << reinterpret_cast<A&>(b).avg << endl;
 
 	return 0;
 }
