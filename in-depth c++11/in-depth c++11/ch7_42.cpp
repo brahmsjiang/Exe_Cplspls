caa4ddf72ef37d37fa08e75b0e27dfd89d25da95
brahmsjiang <brahmsjiang@163.com>
2024-06-26 10:11:49 +0800

    Update ch7_42.cpp

--- in-depth c++11/in-depth c++11/ch7_42.cpp
+++ in-depth c++11/in-depth c++11/ch7_42.cpp
@@ -18,16 +18,25 @@ inline void* aligned_malloc(size_t size, size_t alignment)
 {
 	assert(!(alignment & (alignment - 1)));
 	size_t offset = sizeof(void*) + (--alignment);
-	
-	char* p = static_cast<char*>(malloc(offset + size));
-	if (!p) return nullptr;
+	cout << "sizeof(void*): " << sizeof(void*) << endl;
 
-	void* r = reinterpret_cast<void*>(reinterpret_cast<size_t>(p + offset) & (~alignment));
-	auto ori = static_cast<void**>(r)[-1];
-	auto ori_ptr = (void**)((size_t)r - sizeof(void*));
-	ori = p;
-	auto ori1 = *ori_ptr = p;
-	return r;
+	char* reqBlockAddr = static_cast<char*>(malloc(offset + size));
+	if (!reqBlockAddr) return nullptr;
+
+	void* alignedAddr = reinterpret_cast<void*>(reinterpret_cast<size_t>(reqBlockAddr + offset) & (~alignment));
+	cout << "alignedAddr: " << alignedAddr << endl;
+	auto reqBlockAddrAddr = (void**)((size_t)alignedAddr - sizeof(void*));
+	cout << "reqBlockAddrAddr: " << reqBlockAddrAddr << endl;
+
+	*reqBlockAddrAddr = reqBlockAddr;
+	cout << "reqBlockAddr: " << *reqBlockAddrAddr << endl;
+
+	auto ori = static_cast<void**>(alignedAddr)[-1]; //ori == reqBlockAddr
+	cout << "ori: " << ori << endl;
+
+	//alignedAddr - reqBlockAddr = 4byte (32bit)
+	//alignedAddr - reqBlockAddrAddr = 1byte (8bit)
+	return alignedAddr;
 }
 
 inline void aligned_free(void* p)
