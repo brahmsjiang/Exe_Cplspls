c527e6be5e76c761744daeed8c91e31aaa631c09
brahmsjiang <brahmsjiang@163.com>
2024-07-01 17:42:44 +0800

    Update ch8_2.cpp

--- in-depth c++11/in-depth c++11/ch8_2.cpp
+++ in-depth c++11/in-depth c++11/ch8_2.cpp
@@ -100,6 +100,7 @@ public:
 	template<typename...Args>
 	void Notify(Args&&... args) {
 		for (auto& it : m_connections) {
+			cout << "cur k: " << it.first << endl;
 			it.second(std::forward<Args>(args)...);
 		}
 	}
@@ -122,6 +123,7 @@ private:
 struct stA
 {
 	int a, b;
+	void print() {cout << a << "," << b << endl;}
 	void print(int a, int b) {cout << a << "," << b << endl;}
 };
 void print(int a, int b) {cout << a << "," << b << endl;}
@@ -136,12 +138,12 @@ int main(int argc, const char * argv[]) {
 	subject.Attach(&observer2);
 	subject.Attach(&observer3);
 	subject.Notify();
-	/////////////////////////
+	cout << "/////////////////////////" << endl;
 	Events<std::function<void(int, int)>> myevent;
 	auto key = myevent.Connect(print);
 	stA t;
-	auto lambdakey = myevent.Connect([&t](int a, int b){t.a=a; t.b=b;});
-	std::function<void(int, int)> f = std::bind(&stA::print, &t, std::placeholders::_1, std::placeholders::_2);
+	auto lambdakey = myevent.Connect([&t](int a, int b){t.a=a; t.b=b; t.print();});
+	std::function<void(int, int)> f = std::bind((void(stA::*)(int,int))&stA::print, &t, std::placeholders::_1, std::placeholders::_2);
 	auto bindkey = myevent.Connect(f);
 	int a = 1,b = 2;
 	myevent.Notify(a, b);
