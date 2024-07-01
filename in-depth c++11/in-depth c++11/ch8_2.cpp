00128b3a1685603c66b35d61c975fe920121ee59
brahmsjiang <brahmsjiang@163.com>
2024-07-01 15:29:32 +0800

    update asta & cpp
    update

--- in-depth c++11/in-depth c++11/ch8_2.cpp
+++ in-depth c++11/in-depth c++11/ch8_2.cpp
@@ -17,47 +17,73 @@
 using namespace std;
 
 class Subject;
+class ConcreteSubject;
 
 class Observer {
 public:
-	virtual ~Observer();
-	virtual void Update(Subject* theChangedSubject) = 0;
+	virtual ~Observer() {};
+	virtual void Update() = 0;
 protected:
-	Observer();
+	Observer() {};
 };
-class ConcreteObserver : public Observer {
-	void Update(Subject* theChangedSubject) {
-		cout << "val is: " << theChangedSubject << endl;
-	}
-};
-
 class Subject {
 public:
-	virtual ~Subject();
+	virtual ~Subject() {};
 	virtual void Attach(Observer* o) {
-		_observers->push_back(o);
+		_observers.push_back(o);
 	}
 	virtual void Detach(Observer* o) {
-		_observers->remove(o);
+		_observers.remove(o);
 	}
 	virtual void Notify() {
-		for (auto i = _observers->begin(); i != _observers->end(); i++) {
-			(*i)->Update(this);
+		for (auto i = _observers.begin(); i != _observers.end(); i++) {
+			if (*i != nullptr) {
+				(*i)->Update();
+			}
+			
 		}
 	}
 protected:
-	Subject();
+	Subject() {};
 private:
-	std::list<Observer*> *_observers;//observers list
+	std::list<Observer*> _observers;//observers list
 };
+//////////////////
 class ConcreteSubject : public Subject {
 public:
-	int val = 0;
+	ConcreteSubject() {}
+	~ConcreteSubject() {}
+	int getVal() {
+		return val;
+	}
+private:
+	int val = 2;
 };
-
-
+class ConcreteObserver : public Observer {
+public:
+	ConcreteObserver() {};
+	~ConcreteObserver() {};
+	void setConcreteSubject(ConcreteSubject* subject) {pSubject = subject;}
+	void Update() {
+		if (pSubject != nullptr)
+			cout << "val is: " << pSubject->getVal() << endl;
+		else
+			cout << "mSubject is nullptr" << endl;
+	}
+private:
+	ConcreteSubject* pSubject;
+};
+///////
 
 int main(int argc, const char * argv[]) {
+	ConcreteSubject subject;
+	ConcreteObserver observer1; observer1.setConcreteSubject(&subject);
+	ConcreteObserver observer2; observer2.setConcreteSubject(&subject);
+	ConcreteObserver observer3; observer3.setConcreteSubject(&subject);
+	subject.Attach(&observer1);
+	subject.Attach(&observer2);
+	subject.Attach(&observer3);
+	subject.Notify();
 
 	return 0;
 }
