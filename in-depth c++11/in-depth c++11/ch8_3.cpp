b868bf3ef63e779675db4ae1254bf8fa8ebd468a
brahmsjiang <brahmsjiang@163.com>
2024-07-02 15:17:56 +0800

    update 8_3

--- in-depth c++11/in-depth c++11/ch8_3.cpp
+++ in-depth c++11/in-depth c++11/ch8_3.cpp
@@ -32,13 +32,22 @@ struct Element
 	virtual void Accept(Visitor& visitor) = 0;
 };
 //////////////////
-struct ConcreteVisitor : public Visitor
+struct ConcreteVisitor1 : public Visitor
 {
 	void Visit(ConcreteElement1* element) {
-		cout << "Visit ConcreteElement1" << endl;
+		cout << "ConcreteVisitor1 visit ConcreteElement1" << endl;
 	}
 	void Visit(ConcreteElement2* element) {
-		cout << "Visit ConcreteElement2" << endl;
+		cout << "ConcreteVisitor1 visit ConcreteElement2" << endl;
+	}
+};
+struct ConcreteVisitor2 : public Visitor
+{
+	void Visit(ConcreteElement1* element) {
+		cout << "ConcreteVisitor2 visit ConcreteElement1" << endl;
+	}
+	void Visit(ConcreteElement2* element) {
+		cout << "ConcreteVisitor2 visit ConcreteElement2" << endl;
 	}
 };
 struct ConcreteElement1 : public Element
@@ -55,17 +64,76 @@ struct ConcreteElement2 : public Element
 };
 
 void TestVisitor() {
-	ConcreteVisitor v;
+	ConcreteVisitor1 v1;
+	ConcreteVisitor2 v2;
 	std::unique_ptr<Element> emt1(new ConcreteElement1());
 	std::unique_ptr<Element> emt2(new ConcreteElement2());
-	emt1->Accept(v);
-	emt2->Accept(v);
+	emt1->Accept(v1);
+	emt2->Accept(v1);
+	emt1->Accept(v2);
+	emt2->Accept(v2);
 }
+////////////////////////////////////
+template<typename... Types>
+struct Visitor1;
+template<typename T, typename... Types>
+struct Visitor1<T, Types...> : Visitor1<Types...>
+{
+	using Visitor1<Types...>::Visit;
+	virtual void Visit(const T&) = 0;
+};
+template<typename T>
+struct Visitor1<T>
+{
+	virtual void Visit(const T&) = 0;
+};
+//////////////////
+struct stA;
+struct stB;
+struct Base
+{
+	typedef Visitor1<stA, stB> MyVisitor;
+	virtual void Accept(MyVisitor&) = 0;
+};
+struct stA: Base
+{
+	double val;
+	void Accept(Base::MyVisitor& v) {
+		v.Visit(*this);
+	}
+};
+struct stB: Base
+{
+	int val;
+	void Accept(Base::MyVisitor& v) {
+		v.Visit(*this);
+	}
+};
+struct PrintVisitor: Base::MyVisitor
+{
+	void Visit(const stA& a) {
+		cout << "from stA: " << a.val << endl;
+	}
+	void Visit(const stB& b) {
+		cout << "from stB: " << b.val << endl;
+	}
+};
 
-
+void TestVisitor1() {
+	PrintVisitor vis;
+	stA a;
+	a.val = 8.97;
+	stB b;
+	b.val = 8;
+	Base* base = &a;
+	base->Accept(vis);
+	base = &b;
+	base->Accept(vis);
+}
 
 int main(int argc, const char * argv[]) {
 	TestVisitor();
-
+	cout << "///////////////////" << endl;
+	TestVisitor1();
 	return 0;
 }
