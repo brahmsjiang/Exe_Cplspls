3d9d729abe357fc6d2814107d49ef51566e6173d
brahmsjiang <brahmsjiang@163.com>
2024-07-03 17:40:06 +0800

    update

---
+++ in-depth c++11/in-depth c++11/ch8_4.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <functional>
+#include <type_traits>
+#include <memory>
+#include <vector>
+#include <list>
+#include <typeindex>
+#include <tuple>
+#include <chrono>
+#include <iomanip>
+#include <assert.h>
+#include <algorithm>
+#include <numeric>
+#include <map>
+
+using namespace std;
+
+class Command {
+public:
+	virtual void Execute() = 0;
+};
+
+template<class Receiver>
+class SimpleCommand : public Command {
+public:
+	typedef void(Receiver::* Action)();
+	SimpleCommand(Receiver* r, Action a) : _receiver(r), _action(a) {}
+	virtual void Execute() {
+		(_receiver->*_action)();
+	}
+private:
+	Action _action;
+	Receiver* _receiver;
+};
+///////
+class MyClass {
+public:
+	void Action() {cout << "MyClass::Action" << endl;}	
+};
+void dummy() {
+	MyClass* receiver = new MyClass;
+	Command* aCommand = new SimpleCommand<MyClass>(receiver, &MyClass::Action);
+	aCommand->Execute();
+}
+/////////////////////
+template<typename R=void>
+struct CommCommand {
+private:
+	std::function<R()> m_f;
+public:
+	template<class F, class... Args, class = typename enable_if<!is_member_function_pointer<F>::value>::type>
+	void Wrap(F&& f, Args&&... args) {
+		m_f = [&]{ return f(args...); };
+	}
+	template<class R, class C, class... DArgs, class P, class... Args>
+	void Wrap(R(C::*f)(DArgs...) const, P&& p, Args&&... args) {
+		m_f = [&, f]{ return (*p.*f)(args...); };
+	}
+	template<class R, class C, class... DArgs, class P, class... Args>
+	void Wrap(R(C::*f)(DArgs...), P&& p, Args&&... args) {
+		m_f = [&, f]{ return (*p.*f)(args...); };
+	}
+	R Execute() {
+		return m_f();
+	}
+};
+/*
+struct stA {
+	int m_a;
+	int operator()(){return m_a;}
+	int operator(int n){return m_a + n;}
+	int triple0(){return m_a * 3;}
+	int triple(int a){return m_a * 3 + a;}
+	int triple1() const {return m_a * 3;}
+	const int triple2(int a) const {return m_a * 3 + a;}
+	void triple3() {cout << "" << endl;}
+};
+*/
+int add_one(int n) {
+	return n + 1;
+}
+void TestWrap() {
+	CommCommand<int> cmd;
+}
+
+
+int main(int argc, const char * argv[]) {
+
+	dummy();
+	cout << "/////////////////////" << endl;
+
+	return 0;
+}
