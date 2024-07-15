7b1b8ae49d92a793c77c771dd78c381375be0d94
brahmsjiang <brahmsjiang@163.com>
2024-07-15 11:17:33 +0800

    Update ch10.cpp

--- in-depth c++11/in-depth c++11/ch10.cpp
+++ in-depth c++11/in-depth c++11/ch10.cpp
@@ -56,6 +56,14 @@ void TestProxy() {
 	hello->Output("It is a test");
 }
 ///////////////////////////////////////
+class NonCopyable
+{
+protected:
+	NonCopyable() = default;
+	~NonCopyable() = default;
+	NonCopyable(const NonCopyable&) = delete;
+	NonCopyable& operator=(const NonCopyable&) = delete;
+};
 ///////////////////////////////////////
 #define HAS_MEMBER(member)\
 template<typename T, typename... Args>\
@@ -71,6 +79,49 @@ public:\
 HAS_MEMBER(Foo)
 HAS_MEMBER(After)
 HAS_MEMBER(Before)
+
+template<typename Func, typename... Args>
+struct Aspect : NonCopyable
+{
+public:
+	Aspect(Func&& f) : m_func(std::forward<Func>(f)) {}
+	template<typename T>
+	typename std::enable_if<has_member_Before<T, Args...>::value && has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	{
+		aspect.Before(std::forward<Args>(args)...);
+		m_func(std::forward<Args>(args)...);
+		aspect.After(std::forward<Args>(args)...);
+	}
+	template<typename T>
+	typename std::enable_if<has_member_Before<T, Args...>::value && !has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	{
+		aspect.Before(std::forward<Args>(args)...);
+		m_func(std::forward<Args>(args)...);
+	}
+	template<typename T>
+	typename std::enable_if<!has_member_Before<T, Args...>::value && has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	{
+		m_func(std::forward<Args>(args)...);
+		aspect.After(std::forward<Args>(args)...);
+	}
+	template<typename Head, typename... Tail>
+	void Invoke(Args&&... args, Head&& headAspect, Tail&&... tailAspect)
+	{
+		headAspect.Before(std::forward<Args>(args)...);
+		Invoke(std::forward<Args>(args)..., std::forward<Tail>(tailAspect)...);
+		headAspect.After(std::forward<Args>(args)...);
+	}
+private:
+	Func m_func;
+};
+template<typename T> using identity_t = T;
+
+template<typename... AP, typename... Args, typename Func>
+void Invoke(Func&& f, Args&&... args)
+{
+	Aspect<Func, Args...> asp(std::forward<Func>(f));
+	asp.Invoke(std::forward<Args>(args)..., identity_t<AP>()...);
+}
 
 struct AA {
 	void Before(int i) {
@@ -112,12 +163,33 @@ void HT(int a) {
 }
 void TestAop()
 {
-	auto f = std::bind(HT, std::placeholders::_1);
+	auto f = std::bind(&HT, std::placeholders::_1);
+	Invoke<AA, BB>(std::function<void(int)>(std::bind(&HT, std::placeholders::_1)), 1);
+	Invoke<AA, BB>(f, 1);
+
+	Invoke<CC, DD>(&GT);
+	Invoke<AA, BB>(&HT, 1);
+
+	Invoke<AA, BB>([](int i){}, 1);
+	Invoke<CC, DD>([]{});
 }
 
+/*
+struct TimeElapsedAspect
+{
+	void Before(int i) {
+		m_lastTime = 
+	}
+private:
+	double m_lastTime;
+	Timer m_t;
+}
+*/
 
 int main(int argc, const char * argv[]) {
 	TestProxy();
+	cout << "/////////////////////" << endl;
+	TestAop();
 	cout << "/////////////////////" << endl;
 
 	return 0;
