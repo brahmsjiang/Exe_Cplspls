5bb5546bc461a625790ce01bef257ec6e0652312
brahmsjiang <brahmsjiang@163.com>
2024-07-17 15:53:31 +0800

    Update ch10.cpp

--- in-depth c++11/in-depth c++11/ch10.cpp
+++ in-depth c++11/in-depth c++11/ch10.cpp
@@ -87,6 +87,7 @@ public:
 	template<typename T>
 	typename std::enable_if<has_member_Before<T, Args...>::value && has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
 	{
+		cout << "Invoke(Args&&... args, T&& aspect), Before & After" << endl;
 		aspect.Before(std::forward<Args>(args)...);
 		m_func(std::forward<Args>(args)...);
 		aspect.After(std::forward<Args>(args)...);
@@ -94,18 +95,21 @@ public:
 	template<typename T>
 	typename std::enable_if<has_member_Before<T, Args...>::value && !has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
 	{
+		cout << "Invoke(Args&&... args, T&& aspect), Before" << endl;
 		aspect.Before(std::forward<Args>(args)...);
 		m_func(std::forward<Args>(args)...);
 	}
 	template<typename T>
 	typename std::enable_if<!has_member_Before<T, Args...>::value && has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
 	{
+		cout << "Invoke(Args&&... args, T&& aspect), After" << endl;
 		m_func(std::forward<Args>(args)...);
 		aspect.After(std::forward<Args>(args)...);
 	}
 	template<typename Head, typename... Tail>
 	void Invoke(Args&&... args, Head&& headAspect, Tail&&... tailAspect)
 	{
+		cout << "Invoke(Args&&... args, Head&& headAspect, Tail&&... tailAspect)" << endl;
 		headAspect.Before(std::forward<Args>(args)...);
 		Invoke(std::forward<Args>(args)..., std::forward<Tail>(tailAspect)...);
 		headAspect.After(std::forward<Args>(args)...);
@@ -113,29 +117,30 @@ public:
 private:
 	Func m_func;
 };
+
 template<typename T> using identity_t = T;
 
 template<typename... AP, typename... Args, typename Func>
 void Invoke(Func&& f, Args&&... args)
 {
 	Aspect<Func, Args...> asp(std::forward<Func>(f));
-	asp.Invoke(std::forward<Args>(args)..., identity_t<AP>()...);
+	asp.Invoke(std::forward<Args>(args)..., AP()...); //identity_t<AP>()...
 }
 
 struct AA {
 	void Before(int i) {
-		cout << "Before from AA" << i << endl;
+		cout << "Before from AA," << i << endl;
 	}
 	void After(int i) {
-		cout << "After from AA" << i << endl;
+		cout << "After from AA," << i << endl;
 	}
 };
 struct BB {
 	void Before(int i) {
-		cout << "Before from BB" << i << endl;
+		cout << "Before from BB," << i << endl;
 	}
 	void After(int i) {
-		cout << "After from BB" << i << endl;
+		cout << "After from BB," << i << endl;
 	}
 };
 struct CC {
@@ -164,26 +169,52 @@ void TestAop()
 {
 	auto f = std::bind(&HT, std::placeholders::_1);
 	Invoke<AA, BB>(std::function<void(int)>(std::bind(&HT, std::placeholders::_1)), 1);
+	cout << "**............1st 1st" << endl;
 	Invoke<AA, BB>(f, 1);
-
+	cout << "**............1st 2nd" << endl;
 	Invoke<CC, DD>(&GT);
+	cout << "**............2nd 1st" << endl;
 	Invoke<AA, BB>(&HT, 1);
-
+	cout << "**............2nd 2nd" << endl;
 	Invoke<AA, BB>([](int i){}, 1);
+	cout << "**............3rd 1st" << endl;
 	Invoke<CC, DD>([]{});
+	cout << "**............3rd 2nd" << endl;
 }
 
-/*
+HAS_MEMBER(Foo)
+
 struct TimeElapsedAspect
 {
 	void Before(int i) {
-		m_lastTime = 
+		lastTime = std::chrono::steady_clock::now();
+	}
+	void After(int i) {
+		std::chrono::steady_clock::time_point nowTime = std::chrono::steady_clock::now();
+		cout << "time elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(nowTime - lastTime).count() << "[us]" << endl;
+		lastTime = nowTime;
 	}
 private:
-	double m_lastTime;
-	Timer m_t;
+	std::chrono::steady_clock::time_point lastTime;
+};
+struct LoggingAspect
+{
+	void Before(int i) {
+		cout << "entering..." << std::endl;
+	}
+	void After(int i) {
+		cout << "leaving..." << std::endl;
+	}
+};
+void Foo(int a) {
+	cout << "real HT function: " << a << endl;
 }
-*/
+void TestLoggingAop()
+{
+	Invoke<LoggingAspect, TimeElapsedAspect>(&Foo, 1);
+	cout << "**................." << endl;
+	Invoke<TimeElapsedAspect, LoggingAspect>(&Foo, 1);
+}
 
 template<typename T>
 struct tstCompil
@@ -191,23 +222,14 @@ struct tstCompil
 	static std::true_type tstCompileRetFunc();
 };
 
-template<typename T, typename... Args>
-struct has_member_Foo
-{
-public:
-	static std::false_type Check(int);
-public:
-	enum { value = std::is_same<decltype(Check(0)), std::true_type>::value };
-};
-
 int main(int argc, const char * argv[]) {
-	cout << decltype(has_member_Foo<int>::Check(0))::value << endl;
-	//cout << "tstCompileRetFunc:" << tstCompil<int>::tstCompileRetFunc() << endl;
+	cout << decltype(tstCompil<int>::tstCompileRetFunc())::value << endl;
 	cout << "/////////////////////" << endl;
 	TestProxy();
 	cout << "/////////////////////" << endl;
 	TestAop();
 	cout << "/////////////////////" << endl;
+	TestLoggingAop();
 
 	return 0;
 }
