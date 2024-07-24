45eab4b105eddb64ae1e5a90d2c5ccaf394294e1
brahmsjiang <brahmsjiang@163.com>
2024-07-24 17:18:04 +0800

    update

--- Utilties/any.hpp
+++ Utilties/any.hpp
@@ -1,28 +1,30 @@
 #include <memory>
 #include <typeindex>
+#include <utility>
+#include <ostream>
 
 struct Any
 {
 	Any(void) : m_tpIndex(std::type_index(typeid(void))){}
-	Any(Any& that) : m_ptr(that.Clone()), m_tpIndex(that.m_tpIndex){}
+	Any(const Any& that) : m_ptr(that.Clone()), m_tpIndex(that.m_tpIndex){}
 	Any(Any&& that) : m_ptr(std::move(that.m_ptr)), m_tpIndex(that.m_tpIndex) {}
 	//decay<T>::type, ret decay type of T
 	//is_same<A, B>::value, ret bool
 	//enable_if<bool, T>::type, ret T or no such member depending on bool
 	template<typename U, typename = std::enable_if<!std::is_same<typename std::decay<U>::type, Any>::value, U>::type>
 	Any(U&& value) :
-		m_ptr(new Derived<typename std::decay<U>::type>(forward<U>(value))),
-		m_tpIndex(type_index(typeid(typename std::decay<U>::type))){}
+		m_ptr(new Derived<typename std::decay<U>::type>(std::forward<U>(value))),
+		m_tpIndex(std::type_index(typeid(typename std::decay<U>::type))){}
 	//operator bool in unique_ptr
 	bool IsNull() const { return !bool(m_ptr); }
 	template<class U> bool Is() const {
-		return m_tpIndex == type_index(typeid(U));
+		return m_tpIndex == std::type_index(typeid(U));
 	}
 	template<class U>
 	U& AnyCast() {
 		if (!Is<U>()) {
-			cout << "can't cast" << typeid(U).name() << " to " << m_tpIndex.name() << endl;
-			throw bad_cast();
+			std::cout << "can't cast" << typeid(U).name() << " to " << m_tpIndex.name() << std::endl;
+			throw std::bad_cast();
 		}
 		//down cast
 		auto derived = dynamic_cast<Derived<U>*>(m_ptr.get());
@@ -45,7 +47,7 @@ private:
 	template<typename T>
 	struct Derived : Base {
 		template<typename U>
-		Derived(U&& value) : m_value(forward<U>(value)){}
+		Derived(U&& value) : m_value(std::forward<U>(value)){}
 		BasePtr Clone() const { return BasePtr(new Derived<T>(m_value)); }//deep copy
 		T m_value;
 	};
