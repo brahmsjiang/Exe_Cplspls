e8d698541ec116019fbb957afa024b67b05c0119
brahmsjiang <brahmsjiang@163.com>
2024-08-01 17:19:11 +0800

    Update ch12.cpp

--- in-depth c++11/in-depth c++11/ch12.cpp
+++ in-depth c++11/in-depth c++11/ch12.cpp
@@ -36,7 +36,7 @@ public:
 	using return_type = Ret;
 	using stl_function_type = std::function<function_type>;
 	template<size_t I>
-	struct args {
+	struct args {	//to access the type of the Ith element in a tuple.
 		static_assert(I < arity, "index is out of range, index must less than sizeof Args");
 		using type = typename std::tuple_element<I, std::tuple<Args...>>::type;
 	};
@@ -50,11 +50,17 @@ struct function_traits<Ret(*)(Args...)> : function_traits<Ret(Args...)> {};
 template <typename Ret, typename... Args>
 struct function_traits<std::function<Ret(Args...)>> : function_traits<Ret(Args...)> {};
 
+/*
+  __VA_ARGS__: variadic macro
+  __VA_ARGS__ refers back again to the variable arguments in the macro itself
+  #define DEBUG(...)  printString (__VA_ARGS__)
+  DEBUG("foo", "bar", "baz"); would be replaced with printString ("foo", "bar", "baz")
+*/
+
 ////member func
 #define FUNCTION_TRAITS(...) \
-template <typename RetType, typename ClsType, typename... Args> \
-struct function_traits<RetType(ClsType::*)(Args...) __VA_ARGS__> : function_traits<RetType(Args...)> {};
-
+template <typename Ret, typename ClsType, typename... Args> \
+struct function_traits<Ret(ClsType::*)(Args...) __VA_ARGS__> : function_traits<Ret(Args...)> {};
 FUNCTION_TRAITS()
 FUNCTION_TRAITS(const)
 FUNCTION_TRAITS(volatile)
@@ -62,7 +68,7 @@ FUNCTION_TRAITS(const volatile)
 
 ////func obj
 template<typename Callable>
-struct function_traits : function_traits<decltype(&Callable::operator())> {};
+struct function_traits : function_traits<decltype(&Callable::operator())> {};//why ?
 
 template<typename Function>
 typename function_traits<Function>::stl_function_type to_funcion(const Function& lambda) {
@@ -71,13 +77,31 @@ typename function_traits<Function>::stl_function_type to_funcion(const Function&
 
 template<typename Function>
 typename function_traits<Function>::stl_function_type to_funcion(Function&& lambda) {
-	return static_cast<function_traits<Function>::stl_function_type>(std::forward<Function>(lambda));
+	return static_cast<function_traits<Function>::stl_function_type>(std::forward<Function>(lambda));//perfect forward
 }
 
 template<typename Function>
 typename function_traits<Function>::pointer to_function_pointer(const Function& lambda) {
 	return static_cast<typename function_traits<Function>::pointer>(lambda);
 }
+//////////////
+
+template <typename Ret, typename... Args>
+struct templete_using
+{
+	using signature = Ret(Args...);
+};
+
+
+void testUsing() {
+	using funcWithSig = std::function<templete_using<void, int, std::string>::signature>;
+	funcWithSig obj = [](int num, std::string str){ cout << str << std::to_string(num) << endl; };
+	obj(1, "shit");
+	using anotherSig = void(int, std::string);
+	std::function<anotherSig> obj2 = [](int num, std::string str){ cout << str << std::to_string(num) << endl; };
+	obj(2, "fuck");
+}
+
 
 void test0() {
 	auto f = to_funcion([](int i){return i;});
@@ -87,8 +111,9 @@ void test0() {
 }
 
 int main(int argc, const char * argv[]) {
-	test0();
+	testUsing();
 	cout << "/////////////////////" << endl;
+	test0();
 	cout << "/////////////////////" << endl;
 	cout << "/////////////////////" << endl;
 
